8879c44e622ed86fa0056ca5f630bed8cb856c44
Edison Mera <efmera@gmail.com>
2013-03-22 23:58:21 +0100

    Minor changes

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(rtchecks).
-version('0.1.0').
-title('Run-Time Checking of Assertions for SWI-Prolog').
-keywords([rtchecks]).
+version('0.0.1').
+title('Run-Time Checking for Ciao and SWI-Prolog').
+keywords([assertions,checks,static,dynamic,runtime,analysis]).
 author('Edison Mera Menendez', 'http://edisonm.dyndns.org/').
 packager('Edison Mera Menendez', 'http://edisonm.dyndns.org/').
 maintainer('Edison Mera Menendez', 'http://edisonm.dyndns.org/').
