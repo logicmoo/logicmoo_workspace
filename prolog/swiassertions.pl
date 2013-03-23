a83552517ed1db0a0056b3a6b81d2e34293e94a1
Edison Mera <efmera@gmail.com>
2013-03-23 17:51:58 +0100

    Renamed assertions.pl to swiassertions.pl, a temporary kludge to avoid clashes with the assertions.pl Ciao package

---
+++ prolog/swiassertions.pl
@@ -0,0 +1,15 @@
+:- module(swiassertions, []).
+
+:- use_module(library(compound_expand)).
+:- reexport(library(assertions_op)).
+:- reexport(library(assertions/assrt_lib)).
+
+:- multifile user:file_search_path/2.
+
+user:file_search_path(assertions, library(assertions)).
+
+assrt_lib:nodirective_error_hook(Assr) :-
+    throw(error(context_error(nodirective, Assr), _)).
+
+term_expansion((:- Decl), Records) :-
+    assertion_records(Decl, Records).
