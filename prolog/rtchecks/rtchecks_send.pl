b32bbc9491b42332b25f814d3461b6994150ca6c
Edison Mera <efmera@gmail.com>
2013-03-23 17:59:42 +0100

    Moved send_comp_rtcheck.pl to assertions/send_check.pl in the package assertions

--- prolog/rtchecks/rtchecks_send.pl
+++ prolog/rtchecks/rtchecks_send.pl
@@ -1,11 +1,5 @@
-:- module(rtchecks_send, [send_rtcheck/5, send_comp_rtcheck/3],
-	  [assertions, nortchecks, dcg]).
+:- module(rtchecks_send, [send_rtcheck/5], [assertions, nortchecks, dcg]).
 
 send_rtcheck([], _, _, _, _) :- !.
 send_rtcheck(Props, ErrType, PredName, Dict, AsrLocs) :-
 	send_signal(rtcheck(ErrType, PredName, Dict, Props, AsrLocs)).
-
-send_comp_rtcheck(Goal, PropName, FailName) :-
-	FailName =.. [F|Args],
-	FailProp =.. [F, PredName|Args],
-	send_signal(rtcheck(comp, PredName, [Goal=PredName], [PropName-[FailProp]], [])).
