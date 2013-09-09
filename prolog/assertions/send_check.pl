dd6e31968e8780856c3eceec1eb8e3a140277644
Edison Mera <mera@process-design-center.com>
2013-09-09 15:53:42 +0200

    Implemented get_root_goal/2, to encapsulate getter of the original goal

--- prolog/assertions/send_check.pl
+++ prolog/assertions/send_check.pl
@@ -1,10 +1,17 @@
-:- module(send_check, [send_comp_rtcheck/3]).
+:- module(send_check, [send_comp_rtcheck/3,
+		       get_root_goal/2]).
 
 :- use_module(engine(exceptions)).
+
+get_root_goal(Goal, Goal0) :-
+	( b_getval(rtchecks_goal, Goal0)
+	->true
+	; Goal0 = Goal
+	).
 
 send_comp_rtcheck(Goal, PropName, FailName) :-
 	FailName =.. [F|Args],
 	FailProp =.. [F, PredName|Args],
-	( b_getval(rtchecks_goal, Goal0) -> true ; Goal0 = Goal ),
+	get_root_goal(Goal, Goal0),
 	send_signal(rtcheck(comp, PredName, [Goal0=PredName],
 			    [PropName-[FailProp]], [])).
