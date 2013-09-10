99279a628ac1f00450bf5eb22ce5be13fd044019
Michael Hendricks <michael@ndrix.org>
2013-09-10 10:26:11 -0600

    Group tests by purpose
    As I add more tests, this will help me keep the organized.

--- t/examples.pl
+++ t/examples.pl
@@ -2,15 +2,26 @@
 
 :- use_module(library(tap)).
 
+% character ranges
 deadbeef =~ '[a-f0-9]+'.
 "1973c" =~ '[a-f0-9]+'.
+
+% + meta character
 abbba =~ 'ab+a'.
+
+% * meta character
 aa =~ "ab*a".
+
+% union
 cat =~ 'cat|dog'.
 dog =~ 'cat|dog'.
 pig \~ 'cat|dog'.
+
+% $ meta character
 dog =~ 'dog$'.
 doggie \~ 'dog$'.
+
+% . meta character
 cat =~ 'c.t'.
 cot =~ 'c.t'.
 'c-t' =~ 'c.t'.
