b0234865c46260067b3d796a470eb15e9648cbda
Michael Hendricks <michael@ndrix.org>
2013-09-16 13:18:49 -0600

    Release v0.0.3

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 name(regex).
-version('0.0.2').
+version('0.0.3').
 title('Regular expressions').
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
 home('http://packs.ndrix.com/regex/index.html').
-download('http://packs.ndrix.com/regex/regex-0.0.1.tgz').
+download('http://packs.ndrix.com/regex/regex-0.0.3.tgz').
