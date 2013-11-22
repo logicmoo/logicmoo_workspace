af7c291667bc78a95366e2730cc9dea855db516e
Michael Hendricks <michael@ndrix.org>
2013-11-22 15:09:32 -0700

    Release v0.2.3
    With correct version numbers everywhere this time.

--- pack.pl
+++ pack.pl
@@ -1,8 +1,10 @@
 name(regex).
-version('0.2.2').
 title('Regular expressions').
+
+version('0.2.3').
+download('http://packs.ndrix.com/regex/regex-0.2.3.tgz').
+
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
 home('http://packs.ndrix.com/regex/index.html').
-download('http://packs.ndrix.com/regex/regex-0.2.1.tgz').
