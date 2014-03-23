74aa504f4e117f6cf3717b33ddc01bb3785c94b7
Edison Mera <efmera@gmail.com>
2014-03-23 17:48:21 +0100

    Updated web page

--- pack.pl
+++ pack.pl
@@ -2,8 +2,8 @@ name(assertions).
 version('0.0.1').
 title('Ciao Assertions Reader for SWI-Prolog').
 keywords([assertions,checks,static,dynamic,runtime,analysis]).
-author('Edison Mera Menendez', 'http://edisonm.dyndns.org/').
-packager('Edison Mera Menendez', 'http://edisonm.dyndns.org/').
-maintainer('Edison Mera Menendez', 'http://edisonm.dyndns.org/').
+author('Edison Mera Menendez', 'http://www.edisonm.com/').
+packager('Edison Mera Menendez', 'http://www.edisonm.org/').
+maintainer('Edison Mera Menendez', 'http://www.edisonm.org/').
 home('https://github.com/edisonm/assertions').
 download('https://github.com/edisonm/assertions.git').
