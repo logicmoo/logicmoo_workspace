9867b86f71e0cdbda67ee6ac7fbd82ce487dba59
Edison Mera <efmera@gmail.com>
2014-03-23 17:48:40 +0100

    Updated web page

--- pack.pl
+++ pack.pl
@@ -2,8 +2,8 @@ name(rtchecks).
 version('0.0.1').
 title('Run-Time Checking for Ciao and SWI-Prolog').
 keywords([assertions,checks,static,dynamic,runtime,analysis]).
-author('Edison Mera Menendez', 'http://edisonm.dyndns.org/').
-packager('Edison Mera Menendez', 'http://edisonm.dyndns.org/').
-maintainer('Edison Mera Menendez', 'http://edisonm.dyndns.org/').
+author('Edison Mera Menendez', 'http://www.edisonm.com/').
+packager('Edison Mera Menendez', 'http://www.edisonm.com/').
+maintainer('Edison Mera Menendez', 'http://www.edisonm.com/').
 home('https://github.com/edisonm/rtchecks').
 download('https://github.com/edisonm/rtchecks.git').
