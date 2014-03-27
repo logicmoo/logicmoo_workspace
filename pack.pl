738a925ec7392500dfb41fb60f3c682f72198abf
Edison Mera <efmera@gmail.com>
2014-03-27 20:52:48 +0100

    Added compact_list

---
+++ pack.pl
@@ -0,0 +1,9 @@
+name(xlibrary).
+version('0.0.2').
+title('Extended libraries for Prolog').
+keywords([library]).
+author('Edison Mera Menendez', 'http://www.edisonm.com/').
+packager('Edison Mera Menendez', 'http://www.edisonm.com/').
+maintainer('Edison Mera Menendez', 'http://www.edisonm.com/').
+home('https://github.com/edisonm/xlibrary').
+download('https://github.com/edisonm/xlibrary.git').
