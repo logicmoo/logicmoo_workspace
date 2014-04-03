c40afc16c6d1b0def3ff41e66ff4af6c82eae3dc
Edison Mera <mera@process-design-center.com>
2014-04-03 15:12:51 +0200

    Fixed web page

--- pack.pl
+++ pack.pl
@@ -3,7 +3,7 @@ version('0.0.1').
 title('Ciao Assertions Reader for SWI-Prolog').
 keywords([assertions,checks,static,dynamic,runtime,analysis]).
 author('Edison Mera Menendez', 'http://www.edisonm.com/').
-packager('Edison Mera Menendez', 'http://www.edisonm.org/').
-maintainer('Edison Mera Menendez', 'http://www.edisonm.org/').
+packager('Edison Mera Menendez', 'http://www.edisonm.com/').
+maintainer('Edison Mera Menendez', 'http://www.edisonm.com/').
 home('https://github.com/edisonm/assertions').
 download('https://github.com/edisonm/assertions.git').
