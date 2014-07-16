ac4e013fa13308b715241fa3c13b75c11ad079e3
Edison Mera <mera@process-design-center.com>
2014-07-16 12:25:47 +0200

    Improved implementation of called_from

--- prolog/called_from.pl
+++ prolog/called_from.pl
@@ -1,4 +1,8 @@
-:- module(called_from, [called_from/1, called_from/2, called_from/3]).
+:- module(called_from, [called_from/1,
+			called_from/2,
+			called_from/4,
+			collect_called_from/3,
+			collect_called_from/4]).
 
 :- use_module(library(normalize_head)).
 :- use_module(library(normalize_pi)).
@@ -22,49 +26,55 @@ called_from(Ref) :-
 	called_from(Ref, _).
 
 called_from(Ref, Caller) :-
-    called_from(Ref, Caller, Sorted),
+    called_from(Ref, Caller, [], Sorted),
     maplist(print_call_point, Sorted),
     cleanup_locations(_, dynamic(_, _), _),
     retractall(called_from_db(_, _, _)).
 
-called_from(Ref, Caller, Sorted) :-
-    called_from_meta(Ref, collect_call_point(Caller), Sorted).
+called_from(Ref0, Caller, OptionL, Pairs) :-
+    normalize_head(Ref0, Ref),
+    collect_called_from(Ref, Caller, OptionL, Pairs).
 
-collect_called_from(Ref, OnTrace) :-
+collect_called_from(Ref, Caller, OptionL, Sorted) :-
+    collect_called_from(Ref, Caller, OptionL),
+    findall(L-[PI, CPI], current_called_from(Ref, L, PI, CPI), Pairs),
+    keysort(Pairs, Sorted).
+
+collect_called_from(Ref, Caller, OptionL0) :-
     cleanup_locations(_, dynamic(_, _), _),
     retractall(called_from_db(_, _, _)),
+    merge_options([infer_meta_predicates(false),
+		   autoload(false),
+		   evaluate(false),
+		   trace_reference(Ref),
+		   module_class([user, system, library]),
+		   on_trace(collect_call_point(Caller))],
+		  OptionL0, OptionL),
     setup_call_cleanup(( current_prolog_flag(verbose, Verbose),
 			 set_prolog_flag(verbose, silent)
 		       ),
-		       prolog_walk_code([infer_meta_predicates(false),
-					 autoload(false),
-					 evaluate(false),
-					 trace_reference(Ref),
-					 module_class([user, system, library]),
-					 on_trace(OnTrace)]),
+		       prolog_walk_code(OptionL),
 		       set_prolog_flag(verbose, Verbose)
 		      ).
 
-:- meta_predicate called_from_meta(?,3,-).
-called_from_meta(Ref0, OnTrace, Sorted) :-
-    normalize_head(Ref0, Ref),
-    collect_called_from(Ref, OnTrace),
-    findall(L-Args, current_called_from(Ref, L, Args), Pairs),
-    keysort(Pairs, Sorted).
-
-current_called_from(Call, Loc, [PI, CPI]) :-
+current_called_from(Call, Loc, PI, CPI) :-
     ( called_from_db(From, Call, Caller),
       normalize_pi(Call, PI),
       normalize_pi(Caller, CPI)
     ; Call = M:H,
-      nonvar(H),
-      functor(H, F, A),
       PI = M:F/A,
-      record_locations:declaration_location(PI, dynamic(Type, CPI), From),
-      Type \= def
+      ( nonvar(H)
+      ->functor(H, F, A),
+	record_locations:declaration_location(PI, dynamic(Type, CPI), From),
+	Type \= def
+      ; record_locations:declaration_location(PI, dynamic(Type, CPI), From),
+	Type \= def,
+	functor(H, F, A)
+      )
     ),
     from_location(From, Loc).
 
+:- public collect_call_point/4.
 collect_call_point(Caller, MGoal, Caller, From) :-
     implementation_module(MGoal, IM),
     record_location_dynamic(MGoal, From),
