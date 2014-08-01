4218bed7aa54b865960f1253cc0d9c9fa134fe42
Edison Mera <mera@process-design-center.com>
2014-08-01 15:52:37 +0200

    Added tool to infer the smallest alias for a given file

---
+++ prolog/smallest_alias.pl
@@ -0,0 +1,19 @@
+:- module(smallest_alias, [smallest_alias/2, current_alias/2]).
+
+smallest_alias(File, CAlias) :-
+    findall(s(Size1,Size2)-Alias,
+	    ( current_alias(File, Alias),
+	      Alias =.. [A0, _],
+	      findall(A0,user:file_search_path(A0, _),L),
+	      length(L, Size1),
+	      term_to_atom(Alias, A1),
+	      atom_length(A1, Size2)
+	    ), SA),
+    sort(SA, [_-CAlias|_]).
+
+current_alias(File, Alias) :-
+    user:file_search_path(A0, ADir),
+    absolute_file_name(ADir, Dir, [file_type(directory), solutions(all)]),
+    directory_file_path(Dir, Base, File),
+    file_name_extension(Name, _Ext, Base),
+    Alias =.. [A0, Name].
