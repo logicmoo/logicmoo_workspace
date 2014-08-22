94ed12a956ba1a91bfc529ce51b17e1337fd6367
Edison Mera <mera@process-design-center.com>
2014-08-22 16:07:39 +0200

    Fixed assertion: nortcheck ---> no_rtcheck

--- prolog/foreign/foreign_props.pl
+++ prolog/foreign/foreign_props.pl
@@ -9,24 +9,25 @@
 
 :- use_module(library(swi/assertions)).
 :- use_module(library(swi/basicprops)).
+:- use_module(library(swi/plprops)).
 
-:- prop foreign/1 + nortcheck.
+:- prop foreign/1 + no_rtcheck.
 :- meta_predicate foreign(0).
 foreign(G) :- call(G).
 
-:- prop foreign/2 + nortcheck.
+:- prop foreign/2 + no_rtcheck.
 :- meta_predicate foreign(0,?).
 foreign(G, _) :- call(G).
 
-:- prop returns/2 + nortcheck.
+:- prop returns/2 + no_rtcheck.
 :- meta_predicate returns(0,?).
 returns(G,_) :- call(G).
 
-:- prop returns_state/1 + nortcheck.
+:- prop returns_state/1 + no_rtcheck.
 :- meta_predicate returns_state(0).
 returns_state(G) :- call(G).
 
-:- prop memory_root/1 + nortcheck.
+:- prop memory_root/1 + no_rtcheck.
 :- meta_predicate memory_root(0).
 memory_root(G) :- call(G).
 
