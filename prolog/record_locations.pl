f1799e54c31f281d6968497662c6fd62fc8eef6f
Edison Mera <mera@process-design-center.com>
2014-09-18 12:22:33 +0200

    Fixed record_locations.pl

--- prolog/record_locations.pl
+++ prolog/record_locations.pl
@@ -17,7 +17,13 @@
 
 % Extra location for assertions of a given predicate
 extra_location(Head, M, assertion(Status, Type), From) :-
-    clause(assrt_lib:assertion_head(Head, M, Status, Type, _, _, From), _).
+    clause(assrt_lib:assertion_head(Head, M, Status, Type, _, _, Pos), _, Ref),
+    clause_property(Ref, file(File)),
+    ( nonvar(Pos)
+    ->From = file_term_position(File, Pos)
+    ; clause_property(Ref, line_count(Line)),
+      From = file(File, Line, -1, _)
+    ).
 
 record_extra_location((:- module(M, L)))      :- assert_declaration(export, M, L).
 record_extra_location((:- volatile(L)))       :- assert_declaration(volatile, L).
