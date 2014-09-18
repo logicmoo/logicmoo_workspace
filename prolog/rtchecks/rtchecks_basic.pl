119e02456c1b07fa96c664b1d014ddec306d3814
Edison Mera <mera@process-design-center.com>
2014-09-18 10:19:45 +0200

    Assertions now contains subpositions instead of the locator, it is constructed by adding the clause source file when required

--- prolog/rtchecks/rtchecks_basic.pl
+++ prolog/rtchecks/rtchecks_basic.pl
@@ -89,7 +89,6 @@ get_checkc(call, Props, PropValues, CheckC) :-
 	compound_check_props(instance, Props, CheckProps0),
 	compound_checkc(CheckProps0, PropValues, CheckC).
 
-
 compound_check_props(Check, Props, CheckProps) :-
 	maplist(compound_check_prop(Check), Props, CheckProps).
 
