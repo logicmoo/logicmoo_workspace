b8e9d1be9cc2ac872a94b826061e5c105fe2c8b2
Edison Mera <mera@process-design-center.com>
2014-09-26 15:32:54 +0200

    Added a simple text tracer, to perform automatic tracing of a call

---
+++ prolog/nitrace.pl
@@ -0,0 +1,107 @@
+:- module(nitrace, [nitrace_file/3,
+		    nitrace/3]).
+
+:- meta_predicate nitrace_file(0, +, +).
+
+nitrace_file(Goal, Path, Alias) :-
+    absolute_file_name(Alias, File),
+    setup_call_cleanup(
+	open(File, write, Stream),
+	nitrace(Goal, Path, Stream),
+	close(Stream)).
+
+:- meta_predicate nitrace(0, +, +).
+
+nitrace(Goal, Path, Stream) :-
+    setup_call_cleanup(
+	setup_trace(State, Path, Stream),
+	Goal,
+	cleanup_trace(State)).
+
+setup_trace(state(Visible, Leash, Ref), Path, Stream) :-
+    asserta((user:prolog_trace_interception(Port, Frame, PC, continue)
+	    :- trace_port(Port, Frame, PC, Path, Stream)), Ref),
+    prolog_cover:port_mask([call,
+			    exit,
+			    fail,
+			    redo,
+			    unify,
+			    exception
+			   ], Mask),
+    '$visible'(Visible, Mask),
+    '$leash'(Leash, Mask),
+    trace.
+
+:- multifile user:message_property/2.
+
+user:message_property(stream(Stream),          stream(Stream)) :- !.
+user:message_property(stream(Stream, _, _, _), stream(Stream)) :- !.
+user:message_property(stream(_, File, Line, 0), prefix('~N~w:~d:'-[File, Line])) :- !.
+user:message_property(stream(_, File, Line, N),
+		      prefix('~N~w:~d: +~d'-[File, Line, N])) :- !.
+
+:- multifile skip_trace/1.
+:- dynamic skip_trace/1.
+
+skip_trace(M:_) :-
+    \+ module_property(M, class(user)). % trace only user predicates
+skip_trace(nitrace:_).
+
+trace_port(redo(_), Frame, PC, Path, Stream) :- !,
+    trace_port(redo, Frame, PC, Path, Stream).
+trace_port(Port, Frame, PC0, Path, Stream) :-
+    prolog_frame_attribute(Frame, predicate_indicator, MPI),
+    \+ skip_trace(MPI),
+    ( find_frame_clause(Frame, PC0, PC, 0, N, Cl)
+    ->clause_stream_loc(Cl, Path, Stream, N, StreamLoc)
+    ; StreamLoc = stream(Stream)
+    ), !,
+    print_message(StreamLoc, frame(Frame, Port, PC)).
+trace_port(_, _, _, _, _).
+
+find_frame_clause(Frame, PC, PC, N, N, Cl) :-
+    prolog_frame_attribute(Frame, clause, Cl), !.
+find_frame_clause(Frame, _, PC, N0, N, Cl) :-
+    find_frame_clause_rec(Frame, PC, N0, N, Cl).
+
+find_frame_clause_rec(Frame, PC, N0, N, Cl) :-
+    prolog_frame_attribute(Frame, parent, Parent),
+    ( prolog_frame_attribute(Frame, pc, PC)
+    ->prolog_frame_attribute(Parent, clause, Cl),
+      succ(N0, N)
+    ; succ(N0, N1),
+      find_frame_clause_rec(Parent, PC, N1, N, Cl)
+    ).
+
+find_frame(N, Start, _, PC, Frame) :-
+	N > 0,
+	prolog_frame_attribute(Start, pc, PC0),
+	prolog_frame_attribute(Start, parent, Frame0),
+	NN is N - 1,
+	find_frame2(NN, Frame0, PC0, Frame, PC).
+find_frame(_, Frame, Port, Port, Frame).
+
+find_frame2(0, F, PC, F, PC).
+find_frame2(N, F0, _, F, PC) :-
+	prolog_frame_attribute(F0, parent, F1),
+	prolog_frame_attribute(F0, pc, PC1),
+	NN is N - 1,
+	find_frame2(NN, F1, PC1, F, PC).
+
+clause_stream_loc(Cl, Path, Stream, N, StreamLoc) :-
+    ( clause_property(Cl, file(AFile)),
+      clause_property(Cl, line_count(Line))
+    ->directory_file_path(Path, File, AFile), % trace only files in Path
+      StreamLoc = stream(Stream, File, Line, N)
+    ; StreamLoc = stream(Stream)
+    ).
+
+cleanup_trace(state(Visible, Leash, Ref)) :-
+    nodebug,
+    '$visible'(_, Visible),
+    '$leash'(_, Leash),
+    erase(Ref),
+    !.
+cleanup_trace(_) :-
+    print_message(error, format('Failed when saving tracer data', [])),
+    fail.
