9f330c5245eb2b30c7946cfee01d5080c9320c17
Edison Mera <mera@process-design-center.com>
2014-10-17 11:35:10 +0200

    Predicates in module_files now are more declarative

--- prolog/module_files.pl
+++ prolog/module_files.pl
@@ -27,24 +27,24 @@
     the GNU General Public License.
 */
 
-:- module(module_files, [module_files/2]).
+:- module(module_files, [module_files/2, file_modules/2, module_file/2]).
 
-:- use_module(library(included_files)).
 :- use_module(library(remove_dups)).
 
 % NOTE: Files are not unique, and the first must be the main one
 module_files(M, Files) :-
-    module_file_list(M, UFilesL),
-    append(UFilesL, UFiles),
-    remove_dups(UFiles, Files).
+    findall(File, module_file(M, File), DFiles),
+    remove_dups(DFiles, Files).
 
-module_file_list(M, [Files0|Files]) :-
-    findall(F, module_file_1(M, F), UFiles),
-    remove_dups(UFiles, Files0),
-    included_files(Files0, Files, []).
+file_modules(File, ML) :-
+    findall(M, module_file(M, File), MD),
+    remove_dups(MD, ML).
 
-module_file_1(M, File) :-
+module_file(M, File) :-
     module_property(M, file(File)).
-module_file_1(M, File) :-
+module_file(M, File) :-
     '$load_context_module'(File, M, _),
     \+ module_property(_, file(File)).
+module_file(M, Incl) :-
+    source_file_property(File, includes(Incl, _)),
+    module_file(M, File).
