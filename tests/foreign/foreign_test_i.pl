17f4269331a9a4bc867c212f1daa9f8a9262b1f9
Edison Mera <mera@process-design-center.com>
2014-12-12 17:16:47 +0100

    Minor kludge to be sure that '.' alias is redefined correctly

--- tests/foreign/foreign_test_i.pl
+++ tests/foreign/foreign_test_i.pl
@@ -12,10 +12,13 @@
 :- use_foreign_source(foreign_test).
 :- gen_foreign_library(.('foreign_test_i.so')).
 
-user:file_search_path('.', Dir) :-
+this_dir(Dir) :-
     context_module(M),
     current_module(M, Path),
     directory_file_path(Dir, _, Path).
+
+:- this_dir(Dir),
+    asserta(user:file_search_path('.', Dir)).
 
 :- prop d_t/1 is type.
 
