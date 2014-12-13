b0ba1174588fcc63cee1d8081ac6995fb3974aba
Edison Mera <efmera@gmail.com>
2014-12-13 10:37:44 +0100

    Less duplicated logic in clambda.pl

--- prolog/clambda.pl
+++ prolog/clambda.pl
@@ -1,10 +1,12 @@
 :- module(clambda, [op(201,xfx,+\)]).
 
 :- use_module(library(compound_expand)).
+:- use_module(library(maplist_dcg)).
 
-remove_hats([], G, G).
-remove_hats([V|L], ^(V, G0), G) :-
-    remove_hats(L, G0, G).
+remove_hat(H, ^(H, G), G).
+
+remove_hats(EL) -->
+    maplist_dcg(remove_hat, EL).
 
 cgoal_args(G0, G, AL, EL) :-
     G0 =.. [F|Args],
@@ -13,18 +15,12 @@ cgoal_args(G0, G, AL, EL) :-
 cgoal_args(\,  [G1|EL],    G, [], EL) :- remove_hats(EL, G1, G).
 cgoal_args(+\, [AL,G1|EL], G, AL, EL) :- remove_hats(EL, G1, G).
 
-lambdaize_args(A0, M, VL, Ex0, A) :-
-    length(Ex0, N),
-    (   A0 =.. [F|Args],
-	length(Ex, N),
-	'$append'(Args0, Ex, Args),
-	Ex==Ex0,
-	\+ ( '$member'(E1, Ex),
-	     '$member'(E2, VL),
-	     E1==E2
-	   )
-    ->  A =.. [F|Args0]
-    ;   '$expand':wrap_meta_arguments(A0, M, VL, Ex0, A)
+lambdaize_args(A0, M, VL, Ex, A) :-
+    ( '$member'(E1, Ex),
+      '$member'(E2, VL),
+      E1==E2
+    ->'$expand':wrap_meta_arguments(A0, M, VL, Ex, A)
+    ; '$expand':remove_arg_pos(A0, _, M, VL, Ex, A, _)
     ).
 
 goal_expansion(G0, G) :-
