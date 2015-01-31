d8cedc01152e1dfcd276c16ad3ecda6e71706aca
Edison Mera <efmera@gmail.com>
2015-01-31 17:38:13 +0100

    Removed check_auto.pl, was an attempt to perform checks at the end of compilation of each file, but due to how swi works did not function properly.

--- prolog/audit/check_auto.pl
+++
@@ -1,9 +0,0 @@
-:- module(check_auto, []).
-
-:- use_module(library(audit/audit)).
-
-:- multifile user:term_expansion/2.
-
-user:term_expansion(end_of_file, end_of_file) :-
-	'$set_source_module'(M, M),
-	audit:checkall(M:_).
