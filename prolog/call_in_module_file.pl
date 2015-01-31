ec7fbd11cfb078bf86ca6c8ef5dee92e30d786bd
Edison Mera <efmera@gmail.com>
2015-01-31 10:41:04 +0100

    Fixed minor warnings in call_in_module_file/2

--- prolog/call_in_module_file.pl
+++ prolog/call_in_module_file.pl
@@ -1,8 +1,11 @@
 :- module(call_in_module_file, [call_in_module_file/2]).
 
+:- use_module(library(filesex)).
+:- use_module(library(shell)).
+
 :- meta_predicate call_in_module_file(+,0).
 call_in_module_file(M, G) :-
-    current_module(M, Path),
+    module_property(M, file(Path)),
     working_directory(WD, WD),
     directory_file_path(Dir, _, Path),
     setup_call_cleanup(cd(Dir), G, cd(WD)).
