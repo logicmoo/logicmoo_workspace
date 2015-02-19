862ebe21e5048b0231889067f1c0a1d76e0378dc
Edison Mera <mera@process-design-center.com>
2015-02-19 17:14:39 +0100

    Made interface.pl more robust

--- prolog/interface.pl
+++ prolog/interface.pl
@@ -4,6 +4,9 @@
 
 :- use_module(library(apply)).
 
+:- multifile
+    '$interface'/3.
+
 :- meta_predicate implements(:).
 implements(Context:Alias) :-
     Context:use_module(Alias, []), % Ensure that the module is loaded
@@ -13,28 +16,22 @@ implements(Context:Alias) :-
     module_property(Module, exports(PIL)),
     maplist(Context:export, PIL).
 
+direct_interface(M, F/A) :-
+    \+ current_predicate(M:F/A).
+		 
 :- module_transparent end_interface/0.
 end_interface :-
-    context_module(M),
-    module_property(M, exports(PIL)),
-    forall(( member(F/A, PIL),
-	     \+ current_predicate(M:F/A)
-	   ),
-	   multifile(F/A)). % Just to avoid warnings, will be abolished
+    context_module(Context),
+    module_property(Context, exports(PIL)),
+    partition(interface:direct_interface(Context), PIL, DIL, IIL),
+    compile_aux_clauses(interface:'$interface'(Context, DIL, IIL)),
+    maplist(Context:multifile, DIL). % Just to avoid warnings, will be abolished
 
 bind_interface(Interface, Implementation) :-
-    module_property(Interface, exports(PIL)),
     atom_concat(Interface, '$impl', II),
     current_module(Implementation, File),
-    partition(direct_interface(Interface), PIL, DIL, IIL),
+    '$interface'(Interface, DIL, IIL),
     maplist(Interface:abolish, DIL),
     Interface:use_module(File, DIL),
     maplist(II:abolish, IIL),
     II:use_module(File, IIL).
-
-direct_interface(Interface, F/A) :-
-    current_predicate(Interface:F/A),
-    functor(H, F, A),
-    \+ ( predicate_property(Interface:H, number_of_clauses(N)),
-	 N>0
-       ).
