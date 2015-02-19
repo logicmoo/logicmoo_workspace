98ad1d490860e2c65990fb5c7cb69a4da7687a5e
Edison Mera <mera@process-design-center.com>
2015-02-19 17:48:20 +0100

    Added error throws to interface.pl

--- prolog/interface.pl
+++ prolog/interface.pl
@@ -3,35 +3,48 @@
 		      end_interface/0]).
 
 :- use_module(library(apply)).
+:- use_module(library(error)).
 
 :- multifile
-    '$interface'/3.
+    '$interface'/3,
+    '$implementation'/2.
 
 :- meta_predicate implements(:).
-implements(Context:Alias) :-
-    Context:use_module(Alias, []), % Ensure that the module is loaded
+implements(Implementation:Alias) :-
+    Implementation:use_module(Alias, []), % Ensure that the module is loaded
     absolute_file_name(Alias, File,
 		       [file_type(prolog), access(read)]),
-    current_module(Module, File),
-    module_property(Module, exports(PIL)),
-    maplist(Context:export, PIL).
+    module_property(Interface, file(File)),
+    module_property(Interface, exports(PIL)),
+    compile_aux_clauses(interface:'$implementation'(Implementation, Interface)),
+    maplist(Implementation:export, PIL).
 
 direct_interface(M, F/A) :-
     \+ current_predicate(M:F/A).
 		 
 :- module_transparent end_interface/0.
 end_interface :-
-    context_module(Context),
-    module_property(Context, exports(PIL)),
-    partition(interface:direct_interface(Context), PIL, DIL, IIL),
-    compile_aux_clauses(interface:'$interface'(Context, DIL, IIL)),
-    maplist(Context:multifile, DIL). % Just to avoid warnings, will be abolished
+    context_module(Interface),
+    module_property(Interface, exports(PIL)),
+    partition(interface:direct_interface(Interface), PIL, DIL, IIL),
+    compile_aux_clauses(interface:'$interface'(Interface, DIL, IIL)),
+    maplist(Interface:multifile, DIL). % Just to avoid warnings, will be abolished
 
 bind_interface(Interface, Implementation) :-
-    atom_concat(Interface, '$impl', II),
-    current_module(Implementation, File),
-    '$interface'(Interface, DIL, IIL),
+    ( '$interface'(Interface, DIL, IIL)
+    ->true
+    ; existence_error(interface, Interface)
+    ),
+    ( '$implementation'(Implementation, Interface)
+    ->true
+    ; ( '$implementation'(Implementation, _)
+      ->existence_error(implementation, Implementation)
+      ; existence_error(binding, Interface->Implementation)
+      )
+    ),
+    module_property(Implementation, file(File)),
     maplist(Interface:abolish, DIL),
     Interface:use_module(File, DIL),
+    atom_concat(Interface, '$impl', II),
     maplist(II:abolish, IIL),
     II:use_module(File, IIL).
