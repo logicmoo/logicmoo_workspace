faaeddd689762a113624977658625bcc3b16cdca
Edison Mera <mera@process-design-center.com>
2015-02-26 15:59:19 +0100

    Better swi wrapper for rtchecks package

--- prolog/swi/rtchecks.pl
+++ prolog/swi/rtchecks.pl
@@ -4,18 +4,18 @@
 :- reexport(library(swi/rtchecks_lib)).
 :- use_module(library(compound_expand)).
 :- expects_dialect(ciao).
-:- use_package(rtchecks).
+:- use_module(rtchecks(rtchecks_tr)).
 
 % To allow usage of rtchecks in normal SWI programs:
 
 goal_expansion(Goal0, Goal) :-
     '$set_source_module'(M, M),
-    rtchecks_tr:rtchecks_goal_tr(Goal0, Goal, M),
+    rtchecks_goal_tr(Goal0, Goal, M),
     !.
 
 term_expansion((:- include(library(nativeprops))),
 	       (:- use_module(library(assertions/native_props)))) :- !.
 term_expansion(Term0, Term) :-
     '$set_source_module'(M, M),
-    rtchecks_tr:rtchecks_sentence_tr(Term0, Term, M, []),
+    rtchecks_sentence_tr(Term0, Term, M, []),
     !.
