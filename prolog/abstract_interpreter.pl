f2a1ca36069f6bbf60fd97012171d592cbe75208
Edison Mera <mera@process-design-center.com>
2015-03-02 13:40:16 +0100

    Implemented match_noloops, but is still too slow to be used everywere

--- prolog/abstract_interpreter.pl
+++ prolog/abstract_interpreter.pl
@@ -1,60 +1,169 @@
 :- module(abstract_interpreter, [abstract_interpreter/2,
-				 head_abstraction/3]).
+				 abstract_interpreter/4,
+				 match_head/7,
+				 match_noloops/7]).
 
 :- use_module(library(extra_location)).
+:- use_module(library(term_size)).
 :- use_module(library(implementation_module)).
+:- use_module(library(qualify_meta_goal)).
 
-:- dynamic inferred/3.		% Kind of manual tabling
+:- dynamic inferred/6.
 
-:- meta_predicate abstract_interpreter(0,3).
+:- meta_predicate abstract_interpreter(+,+,7,-).
+abstract_interpreter(Goal, M, Abstraction, data(N, S, Result)) :-
+    setup_call_cleanup(
+       nb_setval(ai_data, 0-[]),
+       ( ( abstract_interpreter(Goal, M, Abstraction, [], [], Result)
+	 *->true
+	 ; Result = fail
+	 ),
+	 nb_getval(ai_data, N-S)
+       ),
+       nb_delete(ai_data)).
 
 abstract_interpreter(M:Goal, Abstraction) :-
-    abstract_interpreter(Goal, M, Abstraction).
+    abstract_interpreter(Goal, M, Abstraction, [], [], _).
 
-abstract_interpreter(Goal, M, _) :-
-    (var(Goal) ; var(M)), !.
-abstract_interpreter(true, _, _) :- !.
-abstract_interpreter(fail, _, _) :- !, fail.
-abstract_interpreter(M:Goal, _, Abs) :-
-    abstract_interpreter(Goal, M, Abs).
-abstract_interpreter(call(Goal), M, Abs) :-
-    abstract_interpreter(Goal, M, Abs).
-abstract_interpreter(\+ A, M, Abs) :-
-    \+ abstract_interpreter(A, M, Abs).
-abstract_interpreter((A, B), M, Abs) :-
-    abstract_interpreter(A, M, Abs),
-    abstract_interpreter(B, M, Abs).
-abstract_interpreter((A;B), M, Abs) :-
-    ( abstract_interpreter(A, M, Abs)
-    ; abstract_interpreter(B, M, Abs)
+abstract_interpreter_body(Goal, M, _, _) -->
+    {var(Goal) ; var(M)}, bottom, !.
+abstract_interpreter_body(M:Goal, _, Abs, State) --> !,
+    abstract_interpreter_body(Goal, M, Abs, State).
+abstract_interpreter_body(call(Goal), M, Abs, State) --> !,
+    abstract_interpreter_body(Goal, M, Abs, State).
+abstract_interpreter_body(\+ A, M, Abs, State) --> !,
+    ( abstract_interpreter_body(A, M, Abs, State)
+    ->bottom %% We can not say that is always true
+    ; []
     ).
-abstract_interpreter(A->B, M, Abs) :-
-    abstract_interpreter(A, M, Abs), % loose of precision
-    abstract_interpreter(B, M, Abs).
+abstract_interpreter_body((A, B), M, Abs, State) --> !,
+    { terms_share(A, B)
+    ->CutOnFail = fail
+    ; CutOnFail = true
+    },
+    abstract_interpreter_body(A, M, Abs, State),
+    ( abstract_interpreter_body(B, M, Abs, State)
+    *->[]
+    ; { CutOnFail = true
+      ->!, fail			% The whole body will fail
+      }
+    ).
+abstract_interpreter_body((A;B), M, Abs, State) --> !,
+    ( abstract_interpreter_body(A, M, Abs, State)
+    ; abstract_interpreter_body(B, M, Abs, State)
+    ).
+abstract_interpreter_body(A->B, M, Abs, State) --> !,
+    {prolog_current_choice(CP)},
+    { terms_share(A, B)
+    ->CutOnFail = fail
+    ; CutOnFail = true
+    },
+    abstract_interpreter_body(A, M, Abs, State),
+    cut_if_no_bottom(CP),	% loose of precision
+    ( abstract_interpreter_body(B, M, Abs, State)
+    *->[]
+    ; { CutOnFail = true
+      ->!, fail
+      }
+    ).
+abstract_interpreter_body(H, M, Abs, State) -->
+    abstract_interpreter(H, M, Abs, State).
 
-abstract_interpreter(Goal, M, Abs) :-
-      % Here we have to loose precision, but ensure termination
-    ( predicate_property(M:Goal, interpreted)
-    ->call(Abs, Goal, M, Body),
+terms_share(A, B) :-
+    term_variables(A, VarsA),
+    term_variables(B, VarsB),
+    ( member(VA, VarsA),
+      member(VB, VarsB),
+      VA==VB
+    ), !.
+
+cut_if_no_bottom(_, bottom, bottom) :- !.
+cut_if_no_bottom(CP) --> {prolog_cut_to(CP)}.
+
+abstract_interpreter(H, M, Abs, State0 ) --> 
+    { predicate_property(M:H, meta_predicate(Meta))
+    ->qualify_meta_goal(M:H, Meta, Goal)
+    ; Goal = H
+    },
+    call(Abs, Goal, M, Body, State0, State),
+    ( {Body = true}
+    ->[]
+    ; {get_context_body(Goal, M, CM)},
+      abstract_interpreter_body(Body, CM, Abs, State)
+    ).
+
+get_context_body(Goal, M, CM) :-
       ( predicate_property(M:Goal, transparent)
       ->CM = M
       ; predicate_property(M:Goal, imported_from(IM))
       ->CM = IM
       ; CM = M
-      ),
-      abstract_interpreter(Body, CM, Abs)
-    ; true % Nothing to said
+      ).
+
+% top: empty set
+% bottom: I don't know, universe set.
+% true: exact result
+
+bottom(_, bottom).
+
+:- multifile match_ai/7.
+
+match_ai(head,    G, M, Body, S0, S) --> match_head(   G, M, Body, S0, S).
+match_ai(noloops, G, M, Body, S0, S) --> match_noloops(G, M, Body, S0, S).
+
+match_head(Goal, M, true, _, _) -->
+    {predicate_property(M:Goal, interpreted)}, !,
+    {match_head_body(Goal, M, Body)},
+    ( {Body = true}
+    ->[]
+    ; bottom %% loose of precision
     ).
+match_head(fail, _, _,    _, _) --> !, {fail}.
+match_head(true, _, true, _, _) --> !, [].
+match_head(!,    _, true, _, _) --> !, [].
+match_head(_,    _, true, _, _) --> bottom.
 
-head_abstraction(Goal, M, true) :-
-    head_body_abstraction(Goal, M, _).
-
-head_body_abstraction(Goal, M, Body) :-
-    ( predicate_property(M:Goal, dynamic),
-      dyn_defined(Goal, M, Body)
+match_head_body(Goal, M, Body) :-
+    ( extra_clauses(Goal, M, Body)
     ; clause(M:Goal, Body)
     ).
 
-dyn_defined(Goal, CM, true) :-
+:- use_module(library(interface), []).
+
+:- multifile extra_clauses/3.
+
+extra_clauses(Goal, CM, true) :-
+    predicate_property(M:Goal, dynamic),
     implementation_module(CM:Goal, M),
     extra_location(Goal, M, dynamic(def, _, _), _).
+extra_clauses(Goal, CM, I:Goal) :-
+    implementation_module(CM:Goal, M),
+    functor(Goal, F, A),
+    ( interface:'$interface'(M, DIL, IIL),
+      ( memberchk(F/A, DIL)
+      ; memberchk(F/A, IIL)
+      )
+    ->interface:'$implementation'(I, M)
+    ).
+
+match_noloops(Goal, M, Body, S, [M:F/A-Size|S]) -->
+    {predicate_property(M:Goal, interpreted)}, !,
+    ( { functor(Goal, F, A),
+	term_size(Goal, Size),
+	\+ ( memberchk(M:F/A-Size1, S),
+	     Size1=<Size
+	   )
+      }
+    ->{ match_head_body(Goal, M, Body) },
+      []
+    ; bottom %% loose of precision
+    ).
+match_noloops(fail, _, _,    S, _) --> !,
+    { nb_getval(ai_data, L-_),
+      length(S, N),
+      ( N > L -> nb_setval(ai_data, N-S) ; true ),
+      fail
+    }.
+match_noloops(true, _, true, S, S) --> !, [].
+match_noloops(!,    _, true, S, S) --> !, [].
+match_noloops(_,    _, true, S, S) --> bottom.
--- prolog/audit/check_trivial_fails.pl
+++ prolog/audit/check_trivial_fails.pl
@@ -4,15 +4,15 @@
 :- use_module(library(location_utils)).
 :- use_module(library(from_utils)).
 :- use_module(library(option_utils)).
-:- use_module(library(extra_location)).
 :- use_module(library(maplist_dcg)).
-:- use_module(library(qualify_meta_goal)).
 :- use_module(library(audit/audit)).
 
 :- multifile
     prolog:message//1.
 
-:- dynamic trivial_fail/2.
+:- dynamic
+    trivial_fail/2,
+    ai_cache_result/2.
 
 audit:check(trivial_fails, Result, OptionL0) :-
     option_allchk(OptionL0, OptionL, FileChk),
@@ -20,98 +20,106 @@ audit:check(trivial_fails, Result, OptionL0) :-
 
 check_trivial_fails(FromChk, OptionL0, Pairs) :-
     select_option(module(M), OptionL0, OptionL1, M),
-    merge_options(OptionL1,
+    select_option(match_ai(MatchAI), OptionL1, OptionL2, match_head),
+    merge_options(OptionL2,
 		  [infer_meta_predicates(false),
 		   autoload(false),
 		   evaluate(false),
-		   trace_reference(_)
+		   trace_reference(_),
+		   module_class([user, system, library])
 		  ], OptionL),
     prolog_walk_code([source(false),
-		      on_trace(collect_trivial_fail_1r(M, FromChk))
+		      on_trace(collect_dynamic_locations(M, FromChk))
 		     |OptionL]),
     prolog_walk_code([source(false),
-		      on_trace(collect_trivial_fail_2r(M, FromChk))
+		      on_trace(collect_trivial_fails(M, FromChk, MatchAI))
 		     |OptionL]),
     findall(CRef, retract(trivial_fail(clause(CRef), _)), ClausesU),
     sort(ClausesU, Clauses),
     ( Clauses==[]
     ->Pairs=[]
     ; prolog_walk_code([clauses(Clauses),
-			on_trace(collect_trivial_fail(M))
+			on_trace(collect_trivial_fails(M, FromChk, MatchAI))
 		       |OptionL]),
       findall(warning-(Loc-Args),
 	      ( retract(trivial_fail(From, Args)),
 		from_location(From, Loc)
 	      ), Pairs)
     ),
+    cleanup_f,
     !.
+
+cleanup_f :-
+    retractall(ai_cache_result(_, _)).
 
 prolog:message(acheck(trivial_fails)) -->
     ['-------------',nl,
      'Trivial Fails',nl,
      '-------------',nl,
-     'The literals below always fails, due to there are no',nl,
-     'matching clauses for such calls', nl, nl].
+     'The literals below always fails, due to there are no', nl,
+     'matching clauses for such calls, which is reported as', nl,
+     'a trivial fail, or because all paths leads to dead', nl,
+     'points, in such case the warning reports also the', nl,
+     'biggest failure chain found', nl, nl].
 prolog:message(acheck(trivial_fails, Loc-Args)) -->
     Loc,
     maplist_dcg(show_trivial_fail, Args).
 
-show_trivial_fail(Arg) -->
-    ['In ~q, trivial fail for ~q'-Arg, nl].
+show_trivial_fail(trivial_fail(Caller, MGoal)) -->
+    ['In ~q, trivial fail for ~q'-[Caller, MGoal], nl].
+show_trivial_fail(failure(Caller, MGoal, S)) -->
+    ['In ~q, failure for ~q, biggest failure chain was ~q'-[Caller, MGoal, S], nl].
 
-:- multifile ignore_predicate/1.
-ignore_predicate(pce_expansion:pce_class(_, _, template, _, _, _)).
-ignore_predicate(pce_host:property(system_source_prefix(_))).
-ignore_predicate(pce_expansion:verbose).
+:- multifile ignore_predicate/2.
+ignore_predicate(H, M) :-
+    predicate_property(M:H, built_in),
+    \+ predicate_property(M:H, dynamic), !.
+ignore_predicate(H, M) :- predicate_property(M:H, multifile), !.
+ignore_predicate(pce_class(_, _, template, _, _, _), pce_expansion).
+ignore_predicate(property(system_source_prefix(_)), pce_host).
+ignore_predicate(verbose, pce_expansion).
 
 :- public
-    collect_trivial_fail_1r/5,
-    collect_trivial_fail_2r/5.
+    collect_dynamic_locations/5,
+    collect_trivial_fails/6.
 
-:- meta_predicate collect_trivial_fail_1r(+,1,+,+,+).
-collect_trivial_fail_1r(M, FromChk, MGoal, _, From) :-
+:- meta_predicate collect_dynamic_locations(+,1,+,+,+).
+collect_dynamic_locations(M, FromChk, MGoal, _, From) :-
     nonvar(MGoal),
     call(FromChk, From),
     record_location_dynamic(MGoal, M, From).
 
-:- meta_predicate collect_trivial_fail_2r(+,1,+,+,+).
-collect_trivial_fail_2r(M, FromChk, MGoal, Caller, From) :-
-    nonvar(MGoal),
+:- meta_predicate collect_trivial_fails(+,1,+,+,+,+).
+collect_trivial_fails(M, FromChk, MatchAI, M:Goal, Caller, From) :-
     call(FromChk, From),
-    collect_trivial_fail(M, MGoal, Caller, From).
-
-collect_trivial_fail(M, MCall, Caller, From) :-
-    record_location_meta(MCall, M, From, all_call_refs, cu_caller_hook(Caller)).
+    record_location_meta(M:Goal, _, From, all_call_refs,
+			 cu_caller_hook(MatchAI, Caller)).
 
 :- use_module(library(abstract_interpreter)).
 
-cu_caller_hook(Caller, M:H, CM, _, _, _, From) :-
+cu_caller_hook(MatchAI, Caller, MGoal, CM, _, _, _, From) :-
     atom(CM),
+    MGoal = M:H,
     callable(H),
-    ( predicate_property(CM:H, interpreted),
-      %% \+ predicate_property(CM:H, dynamic),
-      \+ predicate_property(CM:H, multifile),
-      \+ ignore_predicate(M:H)
-    ->( predicate_property(CM:H, meta_predicate(Meta)) ->
-	qualify_meta_goal(CM:H, Meta, Goal)
-      ; Goal = H
-      ),
-      MGoal = M:Goal,
-      Args = [Caller, MGoal],
-      ( \+ abstract_interpreter(CM:Goal, head_abstraction)
-      ->( \+ ( trivial_fail(From0, Args),
-	       subsumes_from(From, From0 )
-	     )
-	->forall(( trivial_fail(From0, Args), 
-		   subsumes_from(From0, From)
-		 ),
-		 retract(trivial_fail(From0, Args))), % Clean up less precise facts
-	  assertz(trivial_fail(From, Args))
-	; true
-	)
-      ; true
-      )
+    predicate_property(M:H, interpreted),
+    \+ ignore_predicate(H, M),
+    variant_sha1(ai(H, CM), Hash),
+    ( ai_cache_result(Hash, Data) -> true
+    ; once(abstract_interpreter(H, CM, MatchAI, Data)),
+      assertz(ai_cache_result(Hash, Data))
+    ),
+    Data = data(N, S, fail),
+    ( N = 0
+    ->Args = trivial_fail(Caller, MGoal)
+    ; Args = failure(Caller, MGoal, S)
+    ),
+    ( \+ ( trivial_fail(From0, Args),
+	   subsumes_from(From, From0 )
+	 )
+    ->forall(( trivial_fail(From0, Args), 
+	       subsumes_from(From0, From)
+	     ),
+	     retract(trivial_fail(From0, Args))), % Clean up less precise facts
+      assertz(trivial_fail(From, Args))
+    ; true
     ).
-
-dyn_defined(M:Head) :-
-    extra_location(Head, M, dynamic(def, _, _), _).
