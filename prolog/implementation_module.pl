a7308680ba85df36e8d0462d93c771a2933d6a16
Edison Mera <efmera@gmail.com>
2015-03-29 18:24:07 +0200

    Improved comment

--- prolog/implementation_module.pl
+++ prolog/implementation_module.pl
@@ -1,11 +1,10 @@
 :- module(implementation_module, [implementation_module/2]).
 
 :- meta_predicate implementation_module(?,?).
-% BUG: At compile time, if there exist other predicate with the same
-% name in the libraries, and it is called before such predicate be
-% defined, can report the wrong implementation module pointing at the
-% libraries. Work around: Never use names that are already being used
-% in the libraries. --EMM
+% BUG: At compile time, if there exist other predicate with the same name in the
+% libraries, and it is called before such predicate be defined, it can report
+% the wrong implementation module pointing at the libraries. Work around: Never
+% use names that are already being used in the libraries. --EMM
 implementation_module(M:Goal, IM) :-
     ( atom(M),
       callable(Goal),
