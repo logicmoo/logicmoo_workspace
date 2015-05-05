0189095a616d78e772120d2ce9eff93ef231eac4
Edison Mera <efmera@gmail.com>
2015-05-05 15:59:03 +0200

    By default, disable runtime_checks compilation in SWI, is better to use the tracer in rtchecks_trace.pl

--- prolog/swi/rtchecks_lib.pl
+++ prolog/swi/rtchecks_lib.pl
@@ -5,3 +5,4 @@ user:file_search_path(rtchecks, library(rtchecks)).
 :- expects_dialect(ciao).
 :- reexport(rtchecks(rtchecks_rt)).
 :- reexport(rtchecks(rtchecks_utils)).
+:- reexport(rtchecks(rtchecks_eval)).
