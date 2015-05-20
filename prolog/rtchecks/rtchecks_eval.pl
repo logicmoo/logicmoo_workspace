4c37491370d740fa2dd204c3b15cc57f293070b1
Edison Mera <mera@process-design-center.com>
2015-05-20 12:08:05 +0200

    Fixed rtchecks_tracer, to allow meta predicates, by adding breakpoints to abstract machine instructions i_usercall0 and i_usercalln/1.  Ignoring properties, to avoid infinite loops, because properties are intented to be used in the assertion's body and should not be checked.

--- prolog/rtchecks/rtchecks_eval.pl
+++ prolog/rtchecks/rtchecks_eval.pl
@@ -15,16 +15,17 @@ rtchecks_eval(Goal) :-
 
 :- meta_predicate generate_rtchecks(?, 0, -).
 generate_rtchecks(Loc, CM:Goal, CM:RTChecks) :-
-    functor(Goal, F, A),
-    functor(Head, F, A),
-    implementation_module(CM:Goal, M),
     ( proc_ppassertion(Goal, _, [], Loc, RTChecks)
     ->true
-    ; collect_assertions(Head, M, rtcheck, Assertions),
-      Assertions \= [], !,
-      generate_rtchecks(Assertions, Head, M, Loc, G1, G2, G3, CM:Head),
-      qualify_meta_goal(CM:Goal, Head),
-      (M \= CM -> G0 = G1, G2=G3 ; G0 = G3),
-      lists_to_lits(G0, RTChecks)
+    ; functor(Goal, F, A),
+      functor(Head, F, A),
+      implementation_module(CM:Head, M),
+      ( collect_assertions(Head, M, rtcheck, Assertions),
+	Assertions \= []
+      ->generate_rtchecks(Assertions, Head, M, Loc, G1, G2, G3, CM:Head),
+	qualify_meta_goal(CM:Goal, Head),
+	(M \= CM -> G0 = G1, G2=G3 ; G0 = G3),
+	lists_to_lits(G0, RTChecks)
+      ; RTChecks = Goal
+      )
     ).
-generate_rtchecks(_, Goal, Goal).
--- prolog/rtchecks_tracer.pl
+++ prolog/rtchecks_tracer.pl
@@ -1,8 +1,11 @@
 :- module(rtchecks_tracer, [trace_rtc/1]).
 
 :- use_module(library(swi/rtchecks)). % Proper load of ciao dialect modules
+:- use_module(library(implementation_module)).
+:- use_module(library(static_strip_module)).
 :- use_module(library(maplist_dcg)).
 :- use_module(library(ontrace)).
+:- use_module(library(assertions/assrt_lib)).
 :- use_module(rtchecks(rtchecks_rt)).
 :- use_module(rtchecks(rtchecks_gen)).
 :- use_module(rtchecks(rtchecks_eval)).
@@ -85,30 +88,42 @@ rtcheck_port(Port, Frame, Action) :-
 
 rtcheck_port_(unify, Frame, Action) :-
     prolog_frame_attribute(Frame, clause, Clause),
-    ( rtc_scanned(Clause)
-    ->Action = skip
-    ; assertz(rtc_scanned(Clause)),
-      prolog_frame_attribute(Frame, context_module, CM),
-      \+ black_list_module(CM),
-      PI=M:F/A,
-      '$break_pc'(Clause, PC, _NextPC1),
-      '$fetch_vm'(Clause, PC, _NextPC2, TInstr),
-      ( memberchk(TInstr, [i_call(PI), i_depart(PI)]),
-	\+ black_list_module(M),
-	functor(Goal, F, A),
-	( pp_assr(Goal, M)
-	; current_assertion(Goal, M, rtcheck, _)
-	)
-      ->'$break_at'(Clause, PC, true),
-	assertz(rtc_break(Clause, PC)),
-	fail
-      )
-    ).
+    setup_clause_bpt(Clause, Action), !.
 rtcheck_port_(_, Frame, Action) :-
     ( prolog_frame_attribute(Frame, goal, Goal),
       \+ skip_predicate(Goal)
     ->Action = continue
     ; Action = skip
+    ).
+
+setup_clause_bpt(Clause, Action) :-
+    ( rtc_scanned(Clause)
+    ->Action = skip
+    ; assertz(rtc_scanned(Clause)),
+      clause_property(Clause, module(CM)),
+      \+ black_list_module(CM),
+      '$break_pc'(Clause, PC, _NextPC1),
+      '$fetch_vm'(Clause, PC, _NextPC2, TInstr),
+      ( ( memberchk(TInstr, [i_usercall0, i_usercalln(_N)])
+	->nth_clause(M:Goal, _, Clause),
+	  \+ assertion_head_body(Goal, M, _, prop, _, _, _, _, _)
+	; memberchk(TInstr, [i_call(PI), i_depart(PI)]),
+	  ( PI=M:F/A
+	  ->functor(Goal, F, A)
+	  ; PI=F/A
+	  ->functor(Goal, F, A),
+	    implementation_module(CM:Goal, M)
+	  ),
+	  \+ black_list_module(M),
+	  once(( pp_assr(Goal, M)
+	       ; current_assertion(Goal, rtcheck, _, _, _, _,
+				   _, _, _, _, _, _, _, _, _, M)
+	       ))
+	)
+      ->'$break_at'(Clause, PC, true),
+	assertz(rtc_break(Clause, PC)),
+	fail
+      )
     ).
 
 clause_location(Clause, PC, Loc) :-
@@ -123,8 +138,37 @@ prolog:message_location(clause_pc(Clause, PC)) -->
     {clause_location(Clause, PC, Loc)},
     prolog:message_location(Loc).
 
-prolog:break_hook(Clause, PC, FR, _, call(Goal), call(RTChecks)) :-
+% prolog:break_hook(Clause, PC, FR, FBR, Expr, _) :-
+%     writeln(user_error, prolog:break_hook(Clause, PC, FR, FBR, Expr, _)),
+%     fail.
+prolog:break_hook(Clause, PC, FR, _, call(Goal0), Action) :-
     \+ current_prolog_flag(gui_tracer, true),
     rtc_break(Clause, PC),
-    prolog_frame_attribute(FR, context_module, M),
-    generate_rtchecks(clause_pc(Clause, PC), M:Goal, RTChecks).
+    prolog_frame_attribute(FR, context_module, CM),
+    ( black_list_module(CM)
+    ->Action = continue
+    ; resolve_meta_call(Goal0, Goal1),
+      static_strip_module(Goal1, Goal, M, CM),
+      ( black_list_module(M)
+      ->Action = continue
+      ; generate_rtchecks(clause_pc(Clause, PC), M:Goal, RTChecks),
+	Action = call(RTChecks)
+      )
+    ).
+
+% May be this is slow, but it works:
+resolve_meta_call(M:Meta, M:Goal) :- !,
+    resolve_meta_call(Meta, Goal).
+resolve_meta_call(Meta, Goal) :-
+    functor(Meta, call, A),
+    A >= 2, !,
+    Meta =.. [call, Call|Args],
+    extend_args(Call, Args, Goal).
+resolve_meta_call(Goal, Goal).
+
+extend_args(M:Call, Args, M:Goal) :- !,
+    extend_args(Call, Args, Goal).
+extend_args(Call, Args, Goal) :-
+    Call =.. CArgs,
+    append(CArgs, Args, GArgs),
+    Goal =.. GArgs.
