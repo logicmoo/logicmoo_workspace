e79d4377d64a6a0fcff0b6b37c171589ceb9e9b9
Edison Mera <mera@process-design-center.com>
2015-05-26 12:02:56 +0200

    Added location of dynamic creation of clauses to implemented_in.pl

--- prolog/implemented_in.pl
+++ prolog/implemented_in.pl
@@ -1,9 +1,11 @@
-:- module(implemented_in, [implemented_in/1, implemented_in/3]).
+:- module(implemented_in, [implemented_in/1,
+			   implemented_in/3]).
 
 :- use_module(library(lists)).
 :- use_module(library(extra_location)).
 :- use_module(library(prolog_codewalk), []). % for message_location//1
 :- use_module(library(normalize_head)).
+:- use_module(library(audit/audit_codewalk)).
 
 :- multifile
     prolog:message//1,
@@ -13,9 +15,8 @@ prolog:message(acheck(implemented_in(From, Args))) -->
     prolog:message_location(From),
     ['Implements ~w'-Args].
 
-implemented_in(MGoal0, From, Args) :-
-    normalize_head(MGoal0, MGoal),
-    M:Goal = MGoal,
+implemented_in(MGoal, From, Args) :-
+    MGoal = M:Goal,
     functor(Goal, F, A),
     findall(MI, ( current_module(M),
 		  \+ predicate_property(MGoal, imported_from(_)),
@@ -30,18 +31,33 @@ implemented_in(MGoal0, From, Args) :-
 	Declaration \= dynamic(query, _, _)
       ),
       Args = [M:F/A-Declaration]
-    ;
-      Counter=c(1),
-      From = clause(ClauseRef),
-      catch(clause(M:Goal, _, ClauseRef), _, fail),
-      arg(1, Counter, N),
-      Args = [M:F/A-N],
-      N1 is N + 1,
-      nb_setarg(1, Counter, N1)
+    ; From = clause(ClauseRef),
+      catch(( clause(M:Goal, _, ClauseRef),
+	      nth_clause(M:Goal, N, ClauseRef)
+	    ), _, fail),
+      Args = [M:F/A-N]
     ).
 
-implemented_in(Goal) :-
-    implemented_in(Goal, From, Args),
-    print_message(information, acheck(implemented_in(From, Args))),
-    fail.
-implemented_in(_).
+:- dynamic prepared/0.
+
+:- public prepare/0.
+
+prepare :-
+    audit_walk_code([source(false),
+		     infer_meta_predicates(false),
+		     autoload(false),
+		     evaluate(false),
+		     trace_reference(_),
+		     module_class([user, system, library])],
+		    check_trivial_fails:collect_dynamic_locations(M, FromChk),
+		    M,
+		    FromChk),
+    retractall(prepared),
+    assertz(prepared).
+
+implemented_in(MGoal0 ) :-
+    ( prepared -> true ; prepare ),
+    normalize_head(MGoal0, MGoal),
+    M:Goal = MGoal,
+    forall(implemented_in(M:Goal, From, Args),
+	   print_message(information, acheck(implemented_in(From, Args)))).
