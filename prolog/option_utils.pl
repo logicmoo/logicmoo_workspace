14dc7e287a8f3fb5b173d7dea48080d305492339
Edison Mera <mera@process-design-center.com>
2015-06-11 11:03:36 +0200

    In refactoring tools, merged options alias with aliases, dir with dirs, and renamed aliases to files, to have a simpler interface

--- prolog/option_utils.pl
+++ prolog/option_utils.pl
@@ -30,60 +30,45 @@ check_dir_file(Dir, File) :-
     directory_source_files(Dir, FileL, [recursive(true), if(false)]),
     member(File, FileL).
 
-option_alias(File, FileGen0-OptionL0, FileGen-OptionL) :-
-    select_option(alias(Alias), OptionL0, OptionL, Alias),
-    ( nonvar(Alias)
-    ->FileGen0 = ( check_alias(prolog, Alias, File),
-		   FileGen
-		 )
-    ; FileGen0 = FileGen
-    ).
-
-option_aliases(File, FileGen0-OptionL0, FileGen-OptionL) :-
-    select_option(aliases(AliasL), OptionL0, OptionL, AliasL),
-    ( nonvar(AliasL)
-    ->FileGen0 = ( member(Alias, AliasL),
+option_files(File, FileGen0-OptionL0, FileGen-OptionL) :-
+    select_option(files(Files), OptionL0, OptionL, Files),
+    ( is_list(Files)
+    ->FileGen0 = ( member(Alias, Files),
 		   check_alias(prolog, Alias, File),
 		   FileGen
 		 )
-    ; FileGen0 = FileGen
-    ).
-
-option_fdir(File, FileGen0-OptionL0, FileGen-OptionL) :-
-    select_option(dir(Alias), OptionL0, OptionL, Alias),
-    ( nonvar(Alias)
-    ->FileGen0 = ( check_alias(directory, Alias, Dir),
-		   check_dir_file(Dir, File),
+    ; nonvar(Files)
+    ->FileGen0 = ( check_alias(prolog, Files, File),
 		   FileGen
 		 )
     ; FileGen0 = FileGen
     ).
 
 option_fdirs(File, FileGen0-OptionL0, FileGen-OptionL) :-
-    select_option(dirs(AliasL), OptionL0, OptionL, []),
-    ( AliasL \= []
-    ->FileGen0 = ( member(Alias, AliasL),
+    select_option(dirs(Dirs), OptionL0, OptionL, Dirs),
+    ( is_list(Dirs)
+    ->FileGen0 = ( member(Alias, Dirs),
 		   check_alias(directory, Alias, Dir),
+		   check_dir_file(Dir, File),
+		   FileGen
+		 )
+    ; nonvar(Dirs)
+    ->FileGen0 = ( check_alias(directory, Dirs, Dir),
 		   check_dir_file(Dir, File),
 		   FileGen
 		 )
     ; FileGen0 = FileGen
     ).
 
-option_dir(Dir, DirGen0-OptionL0, DirGen-OptionL) :-
-    select_option(dir(Alias), OptionL0, OptionL, Alias),
-    ( nonvar(Alias)
-    ->DirGen0 = ( check_alias(directory, Alias, Dir),
+option_dirs(Dir, DirGen0-OptionL0, DirGen-OptionL) :-
+    select_option(dirs(Dirs), OptionL0, OptionL, Dirs),
+    ( is_list(Dirs)
+    ->DirGen0 = ( member(Alias, Dirs),
+		  check_alias(directory, Alias, Dir),
 		  DirGen
 		)
-    ; DirGen0 = DirGen
-    ).
-
-option_dirs(Dir, DirGen0-OptionL0, DirGen-OptionL) :-
-    select_option(dirs(AliasL), OptionL0, OptionL, []),
-    ( AliasL \= []
-    ->DirGen0 = ( member(Alias, AliasL),
-		  check_alias(directory, Alias, Dir),
+    ; nonvar(Dirs)
+    ->DirGen0 = ( check_alias(directory, Dirs, Dir),
 		  DirGen
 		)
     ; DirGen0 = DirGen
@@ -92,21 +77,16 @@ option_dirs(Dir, DirGen0-OptionL0, DirGen-OptionL) :-
 check_pred(Head, File) :-
     implemented_in(Head, From, _),
     from_to_file(From, File).
-		   
-option_pred(File, FileGen0-OptionL0, FileGen-OptionL) :-
-    select_option(pred(Head), OptionL0, OptionL, []),
-    ( Head \= []
-    ->FileGen0 = ( check_pred(Head, File),
-		   FileGen
-		 )
-    ; FileGen0 = FileGen
-    ).
 
 option_preds(File, FileGen0-OptionL0, FileGen-OptionL) :-
-    select_option(preds(HeadL), OptionL0, OptionL, []),
-    ( Head \= []
+    select_option(preds(HeadL), OptionL0, OptionL, HeadL),
+    ( is_list(HeadL)
     ->FileGen0 = ( member(Head, HeadL),
 		   check_pred(Head, File),
+		   FileGen
+		 )
+    ; nonvar(HeadL)
+    ->FileGen0 = ( check_pred(HeadL, File),
 		   FileGen
 		 )
     ; FileGen0 = FileGen
@@ -159,16 +139,9 @@ option_allchk(M, File) -->
     option_filechk(File).
 
 option_filechk(File) -->
-    option_alias(File),
-    option_aliases(File),
-    option_pred(File),
+    option_files(File),
     option_preds(File),
-    option_fdir(File),
     option_fdirs(File).
-
-option_dir_dirs(Dir) -->
-    option_dir(Dir),
-    option_dirs(Dir).
 
 :- meta_predicate call_2(0,?,?).
 call_2(Goal, File, File) :- call(Goal).
@@ -196,4 +169,4 @@ option_fromchk(OptionL1, OptionL, option_utils:from_chk(FileGen, File)) :-
     option_filechk(File, FileGen-OptionL1, true-OptionL).
 
 option_dirchk(OptionL1, OptionL, option_utils:call_2(DirGen, Dir)) :-
-    option_dir_dirs(Dir, DirGen-OptionL1, true-OptionL).
+    option_dirs(Dir, DirGen-OptionL1, true-OptionL).
