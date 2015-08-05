c46879f723d1373a644a27c5d32b75a4b46f15b6
Edison Mera <mera@process-design-center.com>
2015-08-05 14:20:33 +0200

    Moved resolve_meta_call/2 to its own module, to avoid coupling

---
+++ prolog/resolve_meta_call.pl
@@ -0,0 +1,14 @@
+:- module(resolve_meta_call,
+	  [resolve_meta_call/2]).
+
+:- use_module(library(extend_args)).
+
+% May be this is slow, but it works:
+resolve_meta_call(M:Meta, M:Goal) :- !,
+    resolve_meta_call(Meta, Goal).
+resolve_meta_call(Meta, Goal) :-
+    functor(Meta, call, A),
+    A >= 2, !,
+    Meta =.. [call, Call|Args],
+    extend_args(Call, Args, Goal).
+resolve_meta_call(Goal, Goal).
