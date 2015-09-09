53b0c9260aea797afa250f4f2f3976560493eb57
Edison Mera <mera@process-design-center.com>
2015-09-09 13:41:20 +0200

    Refactorized unfold_calls to allow usage of a metapredicate instead of a goal list, which is faster and remove duplicated logic

--- prolog/unfold_calls.pl
+++ prolog/unfold_calls.pl
@@ -1,6 +1,5 @@
 :- module(unfold_calls,
-	  [unfold_call/3,
-	   unfold_calls/3]).
+	  [unfold_calls/4]).
 
 :- use_module(library(qualify_meta_goal)).
 :- use_module(library(implementation_module)).
@@ -8,31 +7,28 @@
 :- multifile
     unfold_call_hook/4.
 
-:- meta_predicate unfold_calls(0, +, -).
-unfold_calls(CM:Goal, UnfoldL, Calls) :-
+:- meta_predicate unfold_calls(+, +, 2, -).
+unfold_calls(Goal, CM, IsUnfold, Calls) :-
     implementation_module(CM:Goal, M),
-    findall(Call, ( unfold_call(Goal, CM, UnfoldL, [], Call),
+    findall(Call, ( unfold_call(Goal, CM, IsUnfold, [], Call),
 		    \+ M:Goal =@= Call
 		  ), Calls).
 
-:- meta_predicate unfold_call(0, +, -).
-unfold_call(CM:Goal, UnfoldL, Call) :-
-    unfold_call(Goal, CM, UnfoldL, [], Call).
-
 unfold_call(Goal, _, _, _, _) :- var(Goal), !, fail.
 unfold_call(true, _, _, _, _) :- !, fail.
-unfold_call(CM:Goal, _, UnfoldL, NonUnfoldL, Call) :-
+unfold_call(CM:Goal, _, IsUnfold, NonUnfoldL, Call) :-
     nonvar(Goal), !,
-    unfold_call(Goal, CM, UnfoldL, NonUnfoldL, Call).
-unfold_call(Goal0, CM, UnfoldL, NonUnfoldL, Call) :-
-    implementation_module(CM:Goal0, M),
-    qualify_meta_goal(CM:Goal0, Goal),
+    unfold_call(Goal, CM, IsUnfold, NonUnfoldL, Call).
+unfold_call(Goal0, CM, IsUnfold, NonUnfoldL, Call) :-
+    CMGoal0 = CM:Goal0,
+    implementation_module(CMGoal0, M),
+    qualify_meta_goal(CMGoal0, Goal),
     ( unfold_call_hook(Goal, M, CM, Goal2)
     *->
-      unfold_call(Goal2, CM, UnfoldL, NonUnfoldL, Call)
+      unfold_call(Goal2, CM, IsUnfold, NonUnfoldL, Call)
     ; \+ \+ memberchk(M:Goal, NonUnfoldL)
     ->Call = M:Goal
-    ; \+ memberchk(M:Goal, UnfoldL),
+    ; \+ call(IsUnfold, Goal, M),
       \+ ( predicate_property(CM:Goal, meta_predicate(S)),
 	   arg(_, S, 0 )
 	 )
@@ -44,17 +40,17 @@ unfold_call(Goal0, CM, UnfoldL, NonUnfoldL, Call) :-
 	clause_property(Ref, module(BM)),
 	( subsumes_term(Head, Goal)
 	->Goal = Head,
-	  unfold_call(Body, BM, UnfoldL, NonUnfoldL, Call)
+	  unfold_call(Body, BM, IsUnfold, NonUnfoldL, Call)
 	; \+ Head \= Goal
 	->copy_term(Goal, Head),
-	  unfold_call(Body, BM, UnfoldL, [M:Head|NonUnfoldL], Call)
+	  unfold_call(Body, BM, IsUnfold, [M:Head|NonUnfoldL], Call)
 				% Abstraction to get more info
 	)
       ; predicate_property(CM:Goal, meta_predicate(Spec)),
 	( arg(N, Spec, S),
 	  arg(N, Goal, A),
 	  S = 0,
-	  unfold_call(A, CM, UnfoldL, NonUnfoldL, Call)
+	  unfold_call(A, CM, IsUnfold, NonUnfoldL, Call)
 	)
       )
     ).
