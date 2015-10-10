d74fe850ee7034d1dc2cce5537c4978fd934c94a
Edison Mera <efmera@gmail.com>
2015-10-10 13:44:21 +0200

    resolve_meta_call ---> resolve_calln

---
+++ prolog/resolve_calln.pl
@@ -0,0 +1,15 @@
+:- module(resolve_calln,
+	  [resolve_calln/2]).
+
+:- use_module(library(extend_args)).
+
+% May be this is slow, but it works:
+resolve_calln(M:CallN, M:Goal) :- !,
+    resolve_calln(CallN, Goal).
+resolve_calln(Meta, Goal) :-
+    functor(Meta, call, A),
+    A >= 2, !,
+    Meta =.. [call, Call|Args],
+    extend_args(Call, Args, Meta2),
+    resolve_calln(Meta2, Goal).
+resolve_calln(Goal, Goal).
