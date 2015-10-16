3ab67b6a596b0db776f728879eb9f51835f2d428
Michael Hendricks <michael@ndrix.org>
2015-10-16 11:23:54 -0600

    Release v0.3.1

--- History.md
+++ History.md
@@ -1,3 +1,7 @@
+# v0.3.1 (2015-10-16)
+
+  * Fix packaging error
+
 # v0.3.0 (2015-10-16)
 
   * Add support for leading `^` syntax
--- pack.pl
+++ pack.pl
@@ -1,10 +1,10 @@
 name(regex).
 title('Regular expressions').
 
-version('0.3.0').
-download('https://github.com/mndrix/regex/archive/v0.3.0.zip').
+version('0.3.1').
+download('https://github.com/mndrix/regex/archive/v0.3.1.zip').
 
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
-home('https://github.com/mndrix/list_util').
+home('https://github.com/mndrix/regex').
