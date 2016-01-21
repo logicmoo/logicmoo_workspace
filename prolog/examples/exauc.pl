c978c71409dd01ede2650fb4ffea577426ee5f27
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-01-21 10:34:31 +0100

    example queries

--- prolog/examples/exauc.pl
+++ prolog/examples/exauc.pl
@@ -9,3 +9,9 @@ main(AUCROC, ROC, AUCPR, PR):-
   compute_areas_diagrams(
     [0.7-a,0.7-a,0.7 - \+(a),0.6-a,0.6- \+(a),0.5- a,0.4- \+(a)],
     AUCROC, ROC, AUCPR, PR).
+
+/** <examples>
+
+?- main(AUCROC, ROC, AUCPR, PR). 
+
+*/
