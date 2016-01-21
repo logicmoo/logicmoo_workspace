ed30e00972c6adb71f07227d584987c7b0fe173d
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-01-21 12:22:43 +0100

    nices formatting

--- prolog/examples/exauc.pl
+++ prolog/examples/exauc.pl
@@ -7,8 +7,9 @@
 
 main(AUCROC, ROC, AUCPR, PR):-
   compute_areas_diagrams(
-    [0.7-a,0.7-a,0.7 - \+(a),0.6-a,0.6- \+(a),0.5- a,0.4- \+(a)],
+    [0.7 - a, 0.7 - a, 0.7 - \+(a), 0.6 - a, 0.6 - \+(a), 0.5 - a, 0.4 - \+(a)],
     AUCROC, ROC, AUCPR, PR).
+
 
 /** <examples>
 
