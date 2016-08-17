8ac03e0277f5cd398db79a4677ee9b395ca26048
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-08-17 11:00:01 +0200

    matrix inversion

--- prolog/matrix.pl
+++ prolog/matrix.pl
@@ -2,17 +2,106 @@
     [matrix_multiply/3,
     matrix_sum/3,
     dot_product/3,
-    cholesky_decomposition/2
+    cholesky_decomposition/2,
+    matrix_inversion/2,
+    matrix_inv_triang/2
     ]).
 :- use_module(library(clpfd), [transpose/2]).
 
+% http://www.mymathlib.com/c_source/matrices/linearsystems/unit_lower_triangular.c
+% http://www.mcs.csueastbay.edu/~malek/TeX/Triangle.pdf
+
+% ?- matrix_inversion([[2,-1,0],[-1,2,-1],[0,-1,2]],L).
+% L = [[0.7499999999999999, 0.5000000000000001, 0.2500000000000001], [0.5000000000000001, 1.0000000000000004, 0.5000000000000002], [0.2500000000000001, 0.5000000000000002, 0.7500000000000001]].
+% 
+matrix_inversion(A,B):-
+  cholesky_decomposition(A,L),
+  matrix_inv_triang(L,LI),
+  transpose(LI,LIT),
+  matrix_multiply(LIT,LI,B).
+
+% ?- matrix_inv_triang([[2,0,0],[-1,2,0],[0,-1,2]],L).
+% L = [[0.5, 0.0, 0.0], [0.25, 0.5, 0.0], [0.125, 0.25, 0.5]].
+%
+matrix_inv_triang(L1,L2):-
+  get_diagonal(L1,D),
+  maplist(inv,D,ID),
+  diag(ID,IDM),
+  matrix_multiply(IDM,L1,LL1),
+  list_to_term(LL1,LT),
+  length(LL1,N),
+  matrix_inv_i(1,N,LT),
+  term_to_list(LT,N,LL2),
+  matrix_multiply(LL2,IDM,L2).
+
+
+matrix_inv_i(N,N,_LT):-!.
+
+matrix_inv_i(I,N,LT):-
+  matrix_inv_j(0,I,N,LT),
+  I1 is I+1,
+  matrix_inv_i(I1,N,LT).
+
+matrix_inv_j(I,I,_N,_LT):-!.
+
+matrix_inv_j(J,I,N,LT):-
+  get_v(I,J,N,LT,Vij),
+  V_ij is -Vij,
+  set_v(I,J,N,LT,V_ij),
+  J1 is J+1,
+  matrix_inv_k(J1,J,I,N,LT),
+  matrix_inv_j(J1,I,N,LT).
+  
+matrix_inv_k(I,_J,I,_N,_LT):-!.
+
+matrix_inv_k(K,J,I,N,LT):-
+  get_v(I,K,N,LT,Vik),
+  get_v(K,J,N,LT,Vkj),
+  get_v(I,J,N,LT,Vij),
+  NVij is Vij-Vik*Vkj,
+  set_v(I,J,N,LT,NVij),
+  K1 is K+1,
+  matrix_inv_k(K1,J,I,N,LT).
+
+ 
+diag(L,D):-
+  length(L,N),
+  NN is N*N,
+  list0(NN,L0),
+  DT =..[a|L0],
+  N1 is N-1,
+  numlist(0,N1,In),
+  maplist(set_diag(DT,N),In,L),
+  term_to_list(DT,N,D).
+
+set_diag(D,N,I,V):-
+  set_v(I,I,N,D,V).
+
+inv(A,B):-
+  B is 1.0/A.
+
+get_diagonal(L,D):-
+  length(L,N),
+  list_to_term(L,LT),
+  get_diag(0,N,LT,D).
+
+get_diag(N,N,_L,[]):-!.
+
+get_diag(N0,N,L,[H|R]):-
+  get_v(N0,N0,N,L,H),
+  N1 is N0+1,
+  get_diag(N1,N,L,R).
+
+list_to_term(L,LT):-
+  append(L,LL),
+  LT=..[a|LL].
 %%  matrix_multiply(+X,+Y,-M) is det.
 %
 %   X(N*P),Y(P*M),M(N*M)
 %
 matrix_multiply(X,Y,M) :-
   matrix_mul(X,Y,M0),
-  maplist(maplist(is),M0,M).
+  maplist(maplist(is),M,M0).
 
 matrix_mul(X,Y,M) :-
     transpose(Y,T),
