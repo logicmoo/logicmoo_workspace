7fe6565dd78c1777ab915f1e5d7a8be8ac58a03e
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-08-17 13:30:45 +0200

    updated pack

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(matrix).
-title('Porting of the matrix library of Yap Prolog to SWI-Prolog').
+title('Operations with matrices').
 version('1.0').
 author('Fabrizio Riguzzi', 'fabrizio.riguzzi@unife.it').
 
