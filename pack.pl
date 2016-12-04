bf552ddf669cfc855f1d6dee1e472ea0bf7b014a
frnmst/Franco Masotti <franco.masotti@live.com>
2016-12-04 17:51:30 +0100

    Lots of fixes.

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(cplint-r).
 title('R plotting predicates for cplint').
-version('0.1').
+version('0.0').
 author('Franco Masotti','franco.masotti@student.unife.it').
 
--- prolog/cplint_r.pl
+++ prolog/cplint_r.pl
@@ -17,7 +17,8 @@
 
 :- module(cplint_r,
           [ histogram_r/2,
-            density_r/4
+            density_r/4,
+            densities_r/3
           ]).
 
 :- use_module(library(r/r_call)).
@@ -81,48 +82,73 @@ bin_width(Min,Max,NBins,Width) :-
 load_r_libraries :-
     <- library("ggplot2").
     /* Debug purposes */
-    use_rendering(table).
+    /*
+        use_rendering(table).
+        <- df. % name of a data frame.
+    */
 
 r_row(X,Y,r(X,Y)).
 
-xy_from_list(L,R) :-
+get_set_from_list(L,R) :-
     maplist(key,L,X),
     maplist(y,L,Y),
     maplist(r_row,X,Y,R).
 
+
 /* ggplot2 geom wrappers. */
 
 /*
- * FIXME: Change naming of Rserve.tmp.* variables,
- * this was discovered using:
- * <- df.
+ * BinWidth was added because it was recomended on the R documentation,
+ * see docs.ggplot2.org/current/geom_histogram.html
  */
 geom_histogram(L,NBins,BinWidth) :-
     nbinS <- NBins,
     binwidtH <- BinWidth,
-    xy_from_list(L,R),
+    nbinS <- NBins,
+    get_set_from_list(L,R),
     r_data_frame_from_rows(df, R),
-    <- ggplot(data=df,aes(x='Rserve.tmp.0')) + geom_histogram(binwidth=binwidtH,bins=nbinS).
+    colnames(df) <- c("x", "y"),
+    <- ggplot(data=df,aes_string(x="x")) + geom_histogram(bins=nbinS,binwidth=binwidtH).
 
 
-/* FIXME:
- * Add use of Nbins and BinWidth, compare output of R and c3js version to see 
- * what I mean.
- * 
+/*
+ *   + scale_y_log10() 
  */
 geom_density(L) :-
-    xy_from_list(L,R),
+    get_set_from_list(L,R),
     r_data_frame_from_rows(df, R),
-    <- ggplot(data=df,aes(x='Rserve.tmp.0',y='Rserve.tmp.1',group=1)) + geom_line() + geom_point().
+    colnames(df) <- c("x", "y"),
+    <- ggplot(data=df,aes_string(x="x",y="y",group=1)) + geom_line() + geom_point().
 
-/*    <- df.
+
+/*
+ * The numbers seem correct, although the representation slightly differs 
+ * from the one made with c3js.
+ *
+ * Is there a more efficient way of handling the data frame(s)?
+ *
+ * df <- merge(df1,df2,by.x="x"),
+ * <- ggplot(data=df,aes_string(x="x",y="y1",group=1)) + geom_line() + geom_point().
  */
+geom_densities(LPr,LPo) :-
+    get_set_from_list(LPr,R1),
+    get_set_from_list(LPo,R2),
+    r_data_frame_from_rows(df1, R1),
+    r_data_frame_from_rows(df2, R2),
+    colnames(df1) <- c("x", "y1"),
+    colnames(df2) <- c("x", "y2"),
+    df <- data.frame(x=df1$x,y=c(df1$y1,df2$y2),group=rep(c("pre","post"))),
+    <- ggplot(data=df,aes(x=x,y=y,group=group)) + geom_line(aes(color=group)) + geom_point(aes(color=group)).
+    
+
 
 /***************************************** 
  * Plot predicates ***********************
  *****************************************
+ * cd swish/examples/inference ***********
  * grep -l histogram *.pl | less *********
  * grep -l density *.pl | less ***********
+ * grep -l densities *.pl | less *********
  *****************************************/
 
 
@@ -133,7 +159,6 @@ geom_density(L) :-
  * NBins bins. List must be a list of couples of the form [V]-W or V-W
  * where V is a sampled value and W is its weight.
  */
-/* Input controls. Don't change. */
 histogram_r(L0,NBins) :-
     load_r_libraries,
     maplist(to_pair,L0,L1),
@@ -153,10 +178,9 @@ histogram_r(L0,NBins) :-
  */
 histogram_r(L0,NBins,Min,Max) :-
     maplist(to_pair,L0,L1),
-    bin_width(Min,Max,NBins,BinWidth),
     keysort(L1,L),
+    bin_width(Min,Max,NBins,BinWidth),
     geom_histogram(L,NBins,BinWidth).
-
 
 /**
  * density_r(+List:list,+NBins:int,+Min:float,+Max:float) is det
@@ -176,7 +200,30 @@ density_r(Post0,NBins,Min,Max) :-
     geom_density(LPo),
     r_download.
 
-
-
-
+/**
+ * densities_r(+PriorList:list,+PostList:list,+NBins:int) is det
+ *
+ * Draws a line chart of the density of two sets of samples, usually
+ * prior and post observations. The samples from the prior are in PriorList
+ * while the samples from the posterior are in PostList
+ * as couples [V]-W or V-W where V is a value and W its weigth.
+ * The lines are drawn dividing the domain in
+ * NBins bins.
+ */
+densities_r(Pri0,Post0,NBins):-
+  load_r_libraries,
+  maplist(to_pair,Pri0,Pri1),
+  maplist(to_pair,Post0,Post1),
+  maplist(key,Pri1,Pri),
+  maplist(key,Post1,Post),
+  append(Pri,Post,All),
+  max_list(All,Max),
+  min_list(All,Min),
+  bin_width(Min,Max,NBins,BinWidth),
+  keysort(Pri1,Pr),
+  keysort(Post0,Po),
+  bin(NBins,Pr,Min,BinWidth,LPr),
+  bin(NBins,Po,Min,BinWidth,LPo),
+  geom_densities(LPr,LPo),
+  r_download.
 
