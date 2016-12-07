16e8a3d5d883e4765b7574f7a7c475559de2540c
frnmst/Franco Masotti <franco.masotti@live.com>
2016-12-07 17:09:01 +0100

    Updated pack info.

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
-name(cplint-r).
+name('cplint_r').
 title('R plotting predicates for cplint').
 version('0.0.0').
 author('Franco Masotti','franco.masotti@student.unife.it').
 maintainer('Franco Masotti','franco.masotti@student.unife.it').
 packager('Franco Masotti','franco.masotti@student.unife.it').
-home('https://github.com/frnmst/cplint-r').
-download('https://github.com/frnmst/cplint-r/archive/master.tar.gz')
+home('https://github.com/frnmst/cplint_r').
 requires('r_client').
+requires('cplint').
