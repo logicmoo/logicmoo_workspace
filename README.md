963bf3db948f4d0a76de0edb7649936859b93fe6
frnmst/Franco Masotti <franco.masotti@live.com>
2016-12-07 14:46:06 +0100

    Added predicates. Structural fixes.

--- README.md
+++ README.md
@@ -7,6 +7,14 @@ R plotting predicates for cplint
 This library is to be used with Cplint on SWISH
 and it must be placed in the `lib` directory.
 
+## Installation (TODO)
+
+    pack_install('cplint-r').
+
+## Full Documentation
+
+https://frnmst.github.io/cplint-r
+
 ## License
 
 Artistic License 2.0.
--- prolog/cplint_r.pl
+++ prolog/cplint_r.pl
@@ -15,77 +15,45 @@
  * http://www.perlfoundation.org/artistic_license_2_0.
  */
 
+/* Interface */
+
 :- module(cplint_r,
-          [ histogram_r/2,
+          [ mc_sample_bar_r/2,
+            histogram_r/2,
             density_r/4,
             densities_r/3
           ]).
+:-meta_predicate mc_sample_bar_r(:,+).
+
+/* Dependencies */
 
 :- use_module(library(r/r_call)).
 :- use_module(library(r/r_data)).
 :- use_module(swish(r_swish)).
 :- use_module(library(lists)).
+:- use_module(library(pita)).
+:- use_module(library(mcintyre)).
 
 /*********** 
  * Helpers *
  ***********/
 
-
-/* 
- * Lists 
- */
-
-to_pair([E]-W,E-W):- !.
-to_pair(E-W,E-W).
-
-
-key(K-_,K).
-
-
-y(_ - Y,Y).
-
-
-bin(0,_L,_Min,_BW,[]):-!.
-
-bin(N,L,Lower,BW,[V-Freq|T]):-
-  V is Lower+BW/2,
-  Upper is Lower+BW,
-  count_bin(L,Lower,Upper,0,Freq,L1),
-  N1 is N-1,
-  bin(N1,L1,Upper,BW,T).
-
-count_bin([],_L,_U,F,F,[]).
-
-count_bin([H-_W|T0],L,U,F0,F,T):-
-  H<L,!,
-  count_bin(T0,L,U,F0,F,T).
-
-count_bin([H-W|T0],L,U,F0,F,T):-
-  (H>=U->
-    F=F0,
-    T=[H-W|T0]
-  ;
-    F1 is F0+W,
-    count_bin(T0,L,U,F1,F,T)
-  ).
-
-
 bin_width(Min,Max,NBins,Width) :-
   D is Max-Min,
   Width is D/NBins.
 
-
-/* 
- * R
- */
-
 load_r_libraries :-
     <- library("ggplot2").
+
     /* Debug purposes */
     /*
         use_rendering(table).
         <- df. % name of a data frame.
     */
+
+finalize_r_graph :-
+	r_download.
+
 
 r_row(X,Y,r(X,Y)).
 
@@ -95,7 +63,66 @@ get_set_from_list(L,R) :-
     maplist(r_row,X,Y,R).
 
 
-/* ggplot2 geom wrappers. */
+/***************************************** 
+ * Plot predicates ***********************
+ *****************************************
+ * cd swish/examples/inference ***********
+ * grep -l "prob_bar(" *.pl | less *********
+ * grep -l "mc_sample_bar(" *.pl | less *********
+ * grep -l "histogram(" *.pl | less *********
+ * grep -l "density(" *.pl | less ***********
+ * grep -l "densities(" *.pl | less *********
+ *****************************************/
+
+/* mcintyre */
+
+
+geom_sample_bar(PTrue,PFalse) :-
+    L=[PTrue-1,PFalse-1],
+    get_set_from_list(L,R),
+    r_data_frame_from_rows(df1, R),
+    colnames(df1) <- c("prob", "tf"),
+    df <- data.frame(
+        ids=factor(
+            c("T","F")
+        ),
+        probabilities=c(df1$prob)
+    ),
+    <- ggplot(
+        data=df,
+        aes(
+            x=ids,
+            y=probabilities,
+            fill=ids
+        )
+    ) + geom_bar(
+        stat="identity",
+        width=0.05,
+        position=position_dodge()
+    )
+    + coord_flip().
+
+
+/*
+    df <- data.frame(ids=factor(c("T","F"),levels=c("T","F")),probabilities=c(df1$prob)),
+    <- ggplot(data=df,aes(x=ids,y=probabilities)) + geom_bar(stat="identity",width=0.05,position="dodge") + coord_flip().
+*/
+
+/**
+ * mc_sample_bar_r(:Query:atom,+Samples:int) is det
+ *
+ * The predicate samples Query a number of Samples times and returns
+ * a dict for rendering with c3 as a bar chart with
+ * a bar for the number of successes and a bar for the number
+ * of failures.
+ * If Query is not ground, it considers it as an existential query
+ */
+mc_sample_bar_r(M:Goal,S):-
+    load_r_libraries,
+    mc_sample(M:Goal,S,T,F,_P),
+    geom_sample_bar(T,F),
+    finalize_r_graph.
+
 
 /*
  * BinWidth was added because it was recomended on the R documentation,
@@ -104,11 +131,46 @@ get_set_from_list(L,R) :-
 geom_histogram(L,NBins,BinWidth) :-
     nbinS <- NBins,
     binwidtH <- BinWidth,
-    nbinS <- NBins,
     get_set_from_list(L,R),
     r_data_frame_from_rows(df, R),
     colnames(df) <- c("x", "y"),
-    <- ggplot(data=df,aes_string(x="x")) + geom_histogram(bins=nbinS,binwidth=binwidtH).
+    <- ggplot(
+        data=df,
+    	aes_string(x="x")
+    ) + geom_histogram(
+        bins=nbinS,
+        binwidth=binwidtH
+	).
+
+/**
+ * histogram_r(+List:list,+NBins:int) is det
+ *
+ * Draws a histogram of the samples in List dividing the domain in
+ * NBins bins. List must be a list of couples of the form [V]-W or V-W
+ * where V is a sampled value and W is its weight.
+ */
+histogram_r(L0,NBins) :-
+    load_r_libraries,
+    maplist(to_pair,L0,L1),
+    maplist(key,L1,L2),
+    max_list(L2,Max),
+    min_list(L2,Min),
+    histogram_r(L0,NBins,Min,Max),
+    finalize_r_graph.
+
+/**
+ * histogram_r(+List:list,+NBins:int,+Min:float,+Max:float) is det
+ *
+ * Draws a histogram of the samples in List dividing the domain in
+ * NBins bins. List must be a list of couples of the form [V]-W or V-W
+ * where V is a sampled value and W is its weight. The minimum and maximum
+ * values of the domains must be provided.
+ */
+histogram_r(L0,NBins,Min,Max) :-
+    maplist(to_pair,L0,L1),
+    keysort(L1,L),
+    bin_width(Min,Max,NBins,BinWidth),
+    geom_histogram(L,NBins,BinWidth).
 
 
 /*
@@ -119,6 +181,24 @@ geom_density(L) :-
     r_data_frame_from_rows(df, R),
     colnames(df) <- c("x", "y"),
     <- ggplot(data=df,aes_string(x="x",y="y",group=1)) + geom_line() + geom_point().
+
+/**
+ * density_r(+List:list,+NBins:int,+Min:float,+Max:float) is det
+ *
+ * Draws a line chart of the density of a sets of samples.
+ * The samples are in List
+ * as couples [V]-W or V-W where V is a value and W its weigth.
+ * The lines are drawn dividing the domain in
+ * NBins bins.
+ */
+density_r(Post0,NBins,Min,Max) :-
+    load_r_libraries,
+    maplist(to_pair,Post0,Post),
+    bin_width(Min,Max,NBins,BinWidth),
+    keysort(Post,Po),
+    bin(NBins,Po,Min,BinWidth,LPo),
+    geom_density(LPo),
+    finalize_r_graph.
 
 
 /*
@@ -137,68 +217,27 @@ geom_densities(LPr,LPo) :-
     r_data_frame_from_rows(df2, R2),
     colnames(df1) <- c("x", "y1"),
     colnames(df2) <- c("x", "y2"),
-    df <- data.frame(x=df1$x,y=c(df1$y1,df2$y2),group=rep(c("pre","post"))),
-    <- ggplot(data=df,aes(x=x,y=y,group=group)) + geom_line(aes(color=group)) + geom_point(aes(color=group)).
-    
-
-
-/***************************************** 
- * Plot predicates ***********************
- *****************************************
- * cd swish/examples/inference ***********
- * grep -l histogram *.pl | less *********
- * grep -l density *.pl | less ***********
- * grep -l densities *.pl | less *********
- *****************************************/
-
-
-/**
- * histogram_r(+List:list,+NBins:int) is det
- *
- * Draws a histogram of the samples in List dividing the domain in
- * NBins bins. List must be a list of couples of the form [V]-W or V-W
- * where V is a sampled value and W is its weight.
- */
-histogram_r(L0,NBins) :-
-    load_r_libraries,
-    maplist(to_pair,L0,L1),
-    maplist(key,L1,L2),
-    max_list(L2,Max),
-    min_list(L2,Min),
-    histogram_r(L0,NBins,Min,Max),
-    r_download.
-
-/**
- * histogram_r(+List:list,+NBins:int,+Min:float,+Max:float) is det
- *
- * Draws a histogram of the samples in List dividing the domain in
- * NBins bins. List must be a list of couples of the form [V]-W or V-W
- * where V is a sampled value and W is its weight. The minimum and maximum
- * values of the domains must be provided.
- */
-histogram_r(L0,NBins,Min,Max) :-
-    maplist(to_pair,L0,L1),
-    keysort(L1,L),
-    bin_width(Min,Max,NBins,BinWidth),
-    geom_histogram(L,NBins,BinWidth).
-
-/**
- * density_r(+List:list,+NBins:int,+Min:float,+Max:float) is det
- *
- * Draws a line chart of the density of a sets of samples.
- * The samples are in List
- * as couples [V]-W or V-W where V is a value and W its weigth.
- * The lines are drawn dividing the domain in
- * NBins bins.
- */
-density_r(Post0,NBins,Min,Max) :-
-    load_r_libraries,
-    maplist(to_pair,Post0,Post),
-    bin_width(Min,Max,NBins,BinWidth),
-    keysort(Post,Po),
-    bin(NBins,Po,Min,BinWidth,LPo),
-    geom_density(LPo),
-    r_download.
+    df <- data.frame(
+        x=df1$x,
+        y=c(df1$y1,df2$y2),
+        group=rep(c("pre","post"))
+    ),
+    <- ggplot(
+    	data=df,
+    	aes(
+	    	x=x,
+		    y=y,
+    		group=group
+	    )
+	) + geom_line(
+		aes(
+			color=group
+		)
+	) + geom_point(
+		aes(
+			color=group
+		)
+	).
 
 /**
  * densities_r(+PriorList:list,+PostList:list,+NBins:int) is det
@@ -210,20 +249,71 @@ density_r(Post0,NBins,Min,Max) :-
  * The lines are drawn dividing the domain in
  * NBins bins.
  */
-densities_r(Pri0,Post0,NBins):-
-  load_r_libraries,
-  maplist(to_pair,Pri0,Pri1),
-  maplist(to_pair,Post0,Post1),
-  maplist(key,Pri1,Pri),
-  maplist(key,Post1,Post),
-  append(Pri,Post,All),
-  max_list(All,Max),
-  min_list(All,Min),
-  bin_width(Min,Max,NBins,BinWidth),
-  keysort(Pri1,Pr),
-  keysort(Post0,Po),
-  bin(NBins,Pr,Min,BinWidth,LPr),
-  bin(NBins,Po,Min,BinWidth,LPo),
-  geom_densities(LPr,LPo),
-  r_download.
+densities_r(Pri0,Post0,NBins) :-
+    load_r_libraries,
+    maplist(to_pair,Pri0,Pri1),
+    maplist(to_pair,Post0,Post1),
+    maplist(key,Pri1,Pri),
+    maplist(key,Post1,Post),
+    append(Pri,Post,All),
+    max_list(All,Max),
+    min_list(All,Min),
+    bin_width(Min,Max,NBins,BinWidth),
+    keysort(Pri1,Pr),
+    keysort(Post0,Po),
+    bin(NBins,Pr,Min,BinWidth,LPr),
+    bin(NBins,Po,Min,BinWidth,LPo),
+    geom_densities(LPr,LPo),
+    finalize_r_graph.
+
+
+
+
+
+
+
+
+
+/* pita */
+
+/* Build a list composed of two elements so that the predicate is consistant 
+ * with all the others.
+ */
+/*
+geom_prob_bar(PTrue,PFalse) :-
+    L=[PTrue-1,PFalse-1],
+    get_set_from_list(L,R),
+    r_data_frame_from_rows(df1, R),
+    colnames(df1) <- c("prob", "tf"),
+    df <- data.frame(ids=factor(c("t","f"),levels=c("t","f")),probabilities=c(df1$prob)),
+    <- ggplot(data=df,aes(x=ids,y=probabilities)) + geom_bar(stat="identity").
+*/
+
+/**
+ * prob_bar(:Query:atom) is nondet
+ *
+ * The predicate computes the probability of Query
+ * and returns it as a dict for rendering with r as a bar chart with
+ * a bar for the probability of Query true and a bar for the probability of
+ * Query false.
+ *
+ * ----
+ *
+ * If Query is not ground, it returns in backtracking all ground
+ * instantiations of
+ * Query together with their probabilities
+ */
+prob_bar_r(M:Goal) :-
+    load_r_libraries.
+/*
+    s(M:Goal,P),
+    PF is 1.0-P,
+    geom_prob_bar(P,PF),
+    finalize_r_graph.
+*/
+/*
+    PF is 1.0-P == \
+    Probability False = 1 - Probability True. 
+ */
+
 
