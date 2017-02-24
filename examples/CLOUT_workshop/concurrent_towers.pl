b63c16ab44e8b0942fe54234b37b88bc140b38aa
Miguel Calejo <mc@interprolog.com>
2017-02-24 13:42:11 +0000

    Bob's revised examples

--- examples/CLOUT_workshop/concurrent_towers.pl
+++ examples/CLOUT_workshop/concurrent_towers.pl
@@ -1,42 +1,32 @@
+% Note the execution of this program is very sensitive to the order 
+% in which the clauses for make_on and make_clear are written.
+
 maxTime(5).
 fluents location(_,_).
 
 actions move(_,_).
 
-% works very nicely with this initial state which makes the two goals parallel and unrelated
-% initially location(b,a),location(c,b),location(a,floor),
-% location(d,e),location(e,f),location(f,floor).
-
 initially location(f,floor), location(b,f),location(e,b),
 location(a,floor), location(d,a),location(c,d).
 
 if true
-then make_tower([a,b,c,floor]) from _T1 to _T2.
+then make_tower([a,b,c,floor]) from T1 to T2.
 
 if true
-then make_tower([f,e,d,floor]) from _T1 to _T2.
-
-% if true
-% then make_tower([floor]) from _T1 to _T2.
+then make_tower([f,e,d,floor]) from T1 to T2.
 
 clear(Block) at T if Block \= floor,
     not location(_,Block) at T.
-/*
-clear(Block) at T if
-    not location(_,Block) at T.
-*/
-clear(floor) at _.
 
-make_tower([Block,Place|Places]) from T1 to T3 if
-    make_tower([Place|Places]) from T1 to T2,
-    make_on(Block,Place) from T2 to T3.
+clear(floor) at _.
 
 make_tower([Block,floor]) from T1 to T2 if
     make_on(Block,floor) from T1 to T2.
 
-%
-make_tower([floor]) from _T1 to _T2 if true.
-%
+make_tower([Block,Place|Places]) from T1 to T3 if
+	Place \= floor,
+    make_tower([Place|Places]) from T1 to T2,
+    make_on(Block,Place) from T2 to T3.
 
 make_on(Block,Place) from T1 to T4 if
     not location(Block,Place) at T1,
