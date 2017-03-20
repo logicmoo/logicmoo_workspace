6a43b412609ec7e05f8aa42d4ece67c76f6f608b
Miguel Calejo <mc@interprolog.com>
2017-03-20 19:15:39 +0000

    Add Google analytics code

--- swish/user_module_file.pl
+++ swish/user_module_file.pl
@@ -96,7 +96,16 @@ serve_lps_resources(Request) :- % http://localhost:3050/lps/foo/Gruntfile.js wor
 swish_page:swish_resources --> !,
 	swish_css, swish_js, 
 	% {http_absolute_location(lps_resources('lps.css'),LPScss,[])},
-	html_post(head, link([ type('text/css'),rel('stylesheet'),href('/lps/lps.css') ]))
+	html_post(head, link([ type('text/css'),rel('stylesheet'),href('/lps/lps.css') ])),
+	html_post(head, script('
+  (function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');
+
+  ga(\'create\', \'TRACKING_ID_HERE\', \'auto\');
+  ga(\'send\', \'pageview\');
+'))
 ), Expanded), swish_page:asserta(Expanded).
 
 :- expand_term( (
