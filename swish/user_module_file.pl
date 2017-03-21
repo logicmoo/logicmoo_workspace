d0ec6070eaa04de0e6214e707105b595088b4da8
Miguel Calejo <mc@interprolog.com>
2017-03-21 19:49:07 +0000

    Revert "Extract Google Analytics key into separate file"
    This reverts commit 2ea316f2d2ffba7ee373c537a8a663f9ed54862a.

--- swish/user_module_file.pl
+++ swish/user_module_file.pl
@@ -91,22 +91,20 @@ serve_lps_resources(Request) :- % http://localhost:3050/lps/foo/Gruntfile.js wor
 :- use_module('../../swish/lib/page',[swish_resources//0, swish_navbar//1]).
 :- use_module(library(http/html_write)).
 :- use_module(library(http/http_path)).
-:- catch(read_file_to_string(googleAnalyticsKey,Key,[]),_,Key=''), 
-	format(atom(JS),'
+:- expand_term( ( 
+swish_page:swish_resources --> !,
+	swish_css, swish_js, 
+	% {http_absolute_location(lps_resources('lps.css'),LPScss,[])},
+	html_post(head, link([ type('text/css'),rel('stylesheet'),href('/lps/lps.css') ])),
+	html_post(head, script('
   (function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');
 
-  ga(\'create\', \'~w\', \'auto\');
+  ga(\'create\', \'TRACKING_ID_HERE\', \'auto\');
   ga(\'send\', \'pageview\');
-',[Key]),
-	expand_term( ( 
-swish_page:swish_resources --> !,
-	swish_css, swish_js, 
-	% {http_absolute_location(lps_resources('lps.css'),LPScss,[])},
-	html_post(head, link([ type('text/css'),rel('stylesheet'),href('/lps/lps.css') ])),
-	html_post(head, script(JS))
+'))
 ), Expanded), swish_page:asserta(Expanded).
 
 % ... and to remove chat notifications widget, and add LPS site link:
