caeb35806753dbb47d91f04d036e9b7d1e5fed52
Edison Mera <efmera@gmail.com>
2017-04-17 14:40:00 +0200

    Minor change, swapped two clauses for better organization

--- prolog/rtchecks_flags.pl
+++ prolog/rtchecks_flags.pl
@@ -40,8 +40,8 @@ define_flag(rtchecks_check,          [yes, no],        yes).
 define_flag(rtchecks_true,           [yes, no],        no).
 define_flag(rtchecks_false,          [yes, no],        no).
 define_flag(rtchecks_debug,          [yes, no],        no).
-define_flag(rtchecks_trace,          [yes, no],        no).
 define_flag(rtchecks_trust,          [yes, no],        yes).
+define_flag(rtchecks_trace,          [yes, no],        no).
 define_flag(rtchecks_entry,          [yes, no],        yes).
 define_flag(rtchecks_exit,           [yes, no],        yes).
 define_flag(rtchecks_test,           [yes, no],        no).
