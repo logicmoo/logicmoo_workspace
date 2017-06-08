40b419253d700e97e411e26b01ad1a022cb8805a
rzese <riccardo.zese@unife.it>
2017-06-08 15:34:15 +0200

    Description

--- pack.pl
+++ pack.pl
@@ -1,4 +1,4 @@
 name(trill).
-title('A tableau probabilistic reasoner').
+title('A tableau probabilistic reasoner in three different versions').
 version('4.1.0').
 author('Riccardo Zese', 'zsercr@unife.it').
