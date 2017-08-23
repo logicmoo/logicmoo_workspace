29eaee6fcdb6dce690ed187ef68b80ee94739412
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-08-23 18:36:59 +0200

    Updated version

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(rocksdb).
 title('SWI-Prolog interface to RocksDB').
-version('0.6.0').
+version('0.7.0').
 keywords([database, embedded, nosql]).
 author('Jan Wielemaker', 'J.Wielemaker@vu.nl').
 packager( 'Jan Wielemaker', 'J.Wielemaker@cwi.nl' ).
