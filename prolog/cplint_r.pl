aba57d32393d7ba3fd19283ac0543b01c0a69c00
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2018-04-04 18:20:28 +0200

    module pldoc

--- prolog/cplint_r.pl
+++ prolog/cplint_r.pl
@@ -1,21 +1,12 @@
-/* cplint_r.pl
- *
- * Copyright (c) 2016-2017 Franco Masotti (franco.masotti@student.unife.it)
- *
- * This is free software: you can redistribute it and/or modify it under the 
- * terms of the Artistic License 2.0 as published by The Perl Foundation.
- * 
- * This source is distributed in the hope that it will be useful, but WITHOUT 
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
- * FITNESS FOR A PARTICULAR PURPOSE. See the Artistic License 2.0 for more 
- * details.
- *
- * You should have received a copy of the Artistic License 2.0 along the source 
- * as a LICENSE file. If not, obtain it from 
- * http://www.perlfoundation.org/artistic_license_2_0.
- */
+/** <module> cplint_r
 
-/* Interface */
+This module contains predicates for drawing graphs with R in cplint on SWISH.
+
+@author Franco Masotti, Fabrizio Riguzzi
+@license Artistic License 2.0 https://opensource.org/licenses/Artistic-2.0
+@copyright Franco Masotti, Fabrizio Riguzzi
+*/
+
 
 :- module(cplint_r,
           [ build_xy_list/3,
