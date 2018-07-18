16b240935928b9155f6695dce693108ce35b80f1
Mattia Tempestini <mtt.tempestini@gmail.com>
2018-07-18 10:11:14 +0200

    authors

--- pack.pl
+++ pack.pl
@@ -3,3 +3,4 @@ title('Operations with matrices using prolog and c').
 version('1.9.1').
 author('Marco Alberti', 'marco.alberti@unife.it').
 author('Mattia Tempestini', 'mattia.tempestini@student.unife.it').
+author('Fabrizio Riguzzi', 'fabrizio.riguzzi@unife.it').
