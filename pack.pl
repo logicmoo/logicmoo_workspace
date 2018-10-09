7da625abe4b03d4343c047840c068884eec38fe1
Arnaud <ngmrnd@unife.it>
2018-10-09 18:19:53 +0200

    Fixes in pack.pl

--- pack.pl
+++ pack.pl
@@ -2,5 +2,5 @@ name(phil).
 title('Learning Hierarchical Probabilistic Logic Programs parameters with gradient descent and Expectation Maximization').
 version('1.0.0').
 author('Nguembang Fadja Arnaud','arnaud.nguembafadja@unife.it').
-download('https://github.com/ArnaudFadja/PHIL/releases/*.zip').
+download('https://github.com/ArnaudFadja/phil/releases/*.zip').
 
