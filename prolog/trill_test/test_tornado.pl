83726d7b25ac20ee9a3051c885372c7ebeec2db5
rzese <riccardo.zese@unife.it>
2018-11-08 16:02:15 +0100

    updated test

--- prolog/trill_test/test_tornado.pl
+++ prolog/trill_test/test_tornado.pl
@@ -13,7 +13,7 @@ test_tornado:-
     tornado_johnEmployee,
     tornado_peoplePets]).
 
-:- use_module(library(test/trill_test)).
+:- use_module(library(trill_test/trill_test)).
 
 :- begin_tests(tornado_brca, []).
 
