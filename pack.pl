ef5e41d8c192385fc551c627f4444e16fdcb68a4
rzese <riccardo.zese@unife.it>
2018-11-08 16:12:05 +0100

    bddem separated

--- pack.pl
+++ pack.pl
@@ -1,4 +1,6 @@
 name(trill).
 title('A tableau probabilistic reasoner in three different versions').
-version('4.1.0').
+version('5.2.0-beta').
 author('Riccardo Zese', 'zsercr@unife.it').
+download('https://github.com/rzese/trill/releases/*.zip').
+requires(bddem).
