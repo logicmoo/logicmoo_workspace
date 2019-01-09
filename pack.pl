83155d49c24b011c8be5e9c733c7795962925d30
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2019-01-09 12:02:10 +0100

    typo

--- pack.pl
+++ pack.pl
@@ -1,4 +1,4 @@
 name(aleph).
-title('Aleph Inductive Logic Prorgramming system').
+title('Aleph Inductive Logic Programming system').
 version('5').
 author('Fabrizio Riguzzi', 'fabrizio.riguzzi@unife.it').
