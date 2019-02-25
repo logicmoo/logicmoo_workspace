b7fa941b80f8d09ca2bcfe09ca275eba2fc083ac
Miguel Calejo <mc@interprolog.com>
2019-02-25 12:23:35 +0000

    Fix bad reporting of program failures in test suite; fix test result of a Szabo example

--- examples/CLOUT_workshop/SzaboLanguage_insurance_eternal.pl
+++ examples/CLOUT_workshop/SzaboLanguage_insurance_eternal.pl
@@ -1,7 +1,7 @@
 :- include(example('SzaboLanguage_insurance_base.pl')).
 
 % would never ends, because the lawyer allowed foreclosing in any later future... but 
-% the reactive rule in SzaboLanguage_insurance_base.pl added a limit, so this fails
+% the reactive rule in SzaboLanguage_insurance_base.pl added a limit for foreclosing, so this program actually fails
 % observe safeArrival("10 John D. tractors") at "2018-06-21T15:00".
 observe choiceOf("TractoR'Us") at 2018/6/25.
 % observe to("TractoR'Us",usd(120000)) at 2018/6/25.
