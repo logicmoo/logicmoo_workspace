b5be42cceb6028f16ba1ce601816a161e7839f31
Marco Gavanelli <marco.gavanelli@unife.it>
2019-03-21 17:05:16 +0100

    wrong merge of the pack.pl file

--- pack.pl
+++ pack.pl
@@ -1,9 +1,5 @@
 name(sldnfdraw).
 title('SLDNF Draw is a Prolog program that draws SLDNF Trees in LaTeX').
-<<<<<<< HEAD
 version('1.61').
 author('Maarco Gavanelli','marco.gavanelli@unife.it').
-=======
-version('1.6').
-author('Marco Gavanelli','marco.gavanelli@unife.it').
->>>>>>> 4680ca7022e82b2ac917e410dea2a32890d5225b
+
