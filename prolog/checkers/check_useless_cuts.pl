3c80df6f6b11d700478eeffe68c2b26bf4edc74d
Edison Mera <efmera@gmail.com>
2019-04-29 20:23:14 +0200

    Improved precision of useless_cuts analysis by inferring determinism information

--- prolog/checkers/check_useless_cuts.pl
+++ prolog/checkers/check_useless_cuts.pl
@@ -38,7 +38,6 @@
 :- use_module(library(countsols)).
 :- use_module(library(gcu)).
 :- use_module(library(location_utils)).
-:- use_module(library(intercept)).
 :- use_module(library(option_utils)).
 :- use_module(library(checkers/checker)).
 :- use_module(library(check), []).
@@ -46,8 +45,11 @@
 :- multifile
     prolog:message//1.
 
-:- meta_predicate
-    walk_call(+, +, +, 1).
+:- dynamic
+    cut_info/2,
+    inferred_det_db/3,
+    det_checking/1,
+    det_clause/2.
 
 prolog:message(acheck(useless_cuts)) -->
     ['Check useless cuts',nl,
@@ -71,53 +73,166 @@ check_useless_cuts(Options1, Pairs) :-
     cuts_check(M, FromChk, Pairs).
 
 cuts_check(M, FromChk, Pairs) :-
-    findall(warning-Issue, current_det_check(M, FromChk, Issue), Pairs).
+    forall(current_det_check(M, FromChk),
+           true),
+    retractall(cut_info(_, needed)),
+    findall(warning-Issue,
+            collect_useless_cut(Issue, FromChk), Pairs).
+
+collect_useless_cut(useless_cut(Loc, M:F/A-I), FromChk) :-
+    retract(cut_info(Ref, unused)),
+    call(FromChk, clause(Ref)), % Avoid warnings from out there
+    nth_clause(M:H, I, Ref),
+    functor(H, F, A),
+    from_location(clause(Ref), Loc).
 
 % 1. A cut is useless, if is located at the last clause, and the literals above
 % are semidet
 
-current_det_check(M, FromChk, useless_cut(Loc, M:F/A-I)) :-
+current_det_check(M, FromChk) :-
     % PI=M:F/A,
     current_predicate(M:F/A),
     functor(H, F, A),
     MH = M:H,
+    \+ inferred_det_db(H, M, _),
     \+ predicate_property(MH, imported_from(_)),
-    \+ predicate_property(MH, built_in),
-    \+ predicate_property(MH, dynamic),
-    \+ predicate_property(MH, foreign),
-    \+ predicate_property(MH, multifile),
     \+ \+ ( catch(clause(MH, _, Ref), _, fail),
             call(FromChk, clause(Ref))
           ),
-    do_current_det_check(H, M, I, Loc).
+    one_det_check(H, M).
 
-:- dynamic
-    useless_cut_db/1.
+has_clauses(MH) :-
+    \+ predicate_property(MH, built_in),
+    \+ predicate_property(MH, foreign),
+    \+ predicate_property(MH, dynamic),
+    \+ predicate_property(MH, multifile).
 
-do_current_det_check(H, M, I, Loc) :-
-    findall(clause(M:H, B, R), clause(M:H, B, R), ClauseL),
-    intercept(walk_call(ClauseL, H, M, report_useless_cut),
-              useless_cut(Ref),
-              assertz(useless_cut_db(Ref))),
-    retract(useless_cut_db(C)),
-    nth_clause(_, I, C),
-    from_location(clause(C), Loc).
+one_det_check(H, M) :-
+    ( ( predef_det(H, M, Det)
+      ->true
+      ; forall(walk_call(H, M, info), true),
+        infer_det(H, M, Det)
+      )
+    ->assertz(inferred_det_db(H, M, Det))
+    ; print_message(error, format("unexpected failure of infer_def/3 or predef_det/3 ~q", [M:H]))
+    ).
 
-walk_call(ClauseL, H, M, CA) :-
-    prolog_current_choice(CP1),
-    member(clause(M:H, Body, Ref), ClauseL),
-    prolog_current_choice(CP2),
+inferred_det(Lit, M, Det) :-
+    functor(Lit, F, A),
+    functor(H, F, A),
+    predicate_property(M:H, implementation_module(IM)),
+    ( inferred_det_db(H, IM, Det1)
+    ->Det = Det1
+    ; one_det_check(H, IM),
+      inferred_det_db(H, IM, Det)
+    ).
+
+predef_det(H, M, nodet) :-
+    valid_prop_asr(H, M, Asr),
+    member(Prop,
+           [globprops:multi(_),
+            globprops:non_det(_),
+            globprops:nondet(_)
+           ]),
+    prop_asr(glob, Prop, _, Asr),
+    !.
+predef_det(H, M, isdet) :-
+    once(valid_prop_asr(H, M, _)),
+    forall(valid_prop_asr(H, M, Asr),
+           ( member(Prop,
+                    [globprops:det(_),
+                     globprops:semidet(_),
+                     globprops:is_det(_),
+                     globprops:no_choicepoints(_)
+                    ]),
+             prop_asr(glob, Prop, _, Asr)
+           )),
+    !.
+predef_det(H, M, fails) :-
+    once(valid_prop_asr(H, M, _)),
+    forall(valid_prop_asr(H, M, Asr),
+           ( member(Prop,
+                    [globprops:fails(_),
+                     globprops:failure(_)
+                    ]),
+             prop_asr(glob, Prop, _, Asr)
+           )).
+predef_det(H, M, nodet) :-
+    \+ has_clauses(M:H),
+    !.
+predef_det(H, M, fails) :-
+    predicate_property(M:H, number_of_clauses(0 )),
+    !.
+
+infer_det(H, M, Det) :-
+    member(Det, [multi, isdet, fails]),
+    clause(M:H, _, Ref),
+    det_clause(Ref, Det),
+    !.
+
+walk_call(H, M, CA) :-
+    ( \+ ( clause(M:H, _, Ref),
+           det_checking(Ref)
+         )
+    ->del_det_clause(CA, H, M),
+      prolog_current_choice(CP1),
+      clause(M:H, Body, Ref),
+      prolog_current_choice(CP2),
+      add_neg_clause(CA, Ref),
+      clause_property(Ref, module(CM)),
+      setup_call_cleanup(
+          assertz(det_checking(Ref)),
+          do_walk_body(Body, CM, Ref, CA, CP1, CP2),
+          retractall(det_checking(Ref))),
+      prolog_cut_to(CP2)
+    ; ( true
+      ; fail
+      )
+    ).
+
+do_walk_body(Body, CM, Ref, CA, CP1, CP2) :-
+    prolog_current_choice(CP3),
     ( CP1 == CP2
     ->true
     ; ( true
       ; fail
       )
     ),
-    walk_body(Body, M, Ref, CA, CP2).
+    walk_body(Body, CM, Ref, CA, CP3),
+    add_det_clause(CA, Ref, CP3).
 
-report_useless_cut(Ref) :- send_signal(useless_cut(Ref)).
+% TBD: distinguish between different cuts in a given clause
+add_cut_info(noop, _, _).
+add_cut_info(info, Ref, Info) :-
+    add_cut_info(Info, Ref).
 
-noop(_).
+add_cut_info(unused, Ref) :-
+    ( cut_info(Ref, _)
+    ->true
+    ; assertz(cut_info(Ref, unused))
+    ).
+add_cut_info(needed, Ref) :-
+    retractall(cut_info(Ref, _)),
+    assertz(cut_info(Ref, needed)).
+
+del_det_clause(info, H, M) :-
+    forall(clause(M:H, _, Ref),
+           retractall(det_clause(Ref, _))).
+del_det_clause(noop, _, _).
+
+add_det_clause(info, Ref, CP2) :-
+    prolog_current_choice(CP3),
+    ( CP2 == CP3
+    ->DetInfo = isdet
+    ; DetInfo = multi
+    ),
+    retractall(det_clause(Ref, _)),
+    assertz(det_clause(Ref, DetInfo)).
+add_det_clause(noop, _, _).
+
+add_neg_clause(info, Ref) :-
+    assertz(det_clause(Ref, fails)).
+add_neg_clause(noop, _).
 
 valid_prop_asr(Lit, M, Asr) :-
     prop_asr(Lit, M, Status, Type, _, _, Asr),
@@ -153,8 +268,10 @@ walk_body_if_branch(C, M, Ref, CA, SFlag, CP1, CP2) :-
     ),
     fail.
 
-walk_body(V, _, _, _, _) :-
-    var(V),
+walk_body(V, M, _, _, _) :-
+    ( var(V)
+    ; var(M)
+    ),
     !,
     ( true
     ; fail
@@ -162,10 +279,12 @@ walk_body(V, _, _, _, _) :-
 walk_body(!, _, Ref, CA, CP1) :-
     !,
     prolog_current_choice(CP),
-    ( CP1 == CP
-    ->call(CA, Ref)
-    ; prolog_cut_to(CP1)
-    ).
+    ( CP1 \= CP
+    ->Info = needed,
+      prolog_cut_to(CP1)
+    ; Info = unused
+    ),
+    add_cut_info(CA, Ref, Info).
 walk_body(M:Lit, _, Ref, CA, CP) :-
     !,
     walk_body(Lit, M, Ref, CA, CP).
@@ -173,26 +292,30 @@ walk_body((A, B), M, Ref, CA, CP) :-
     !,
     walk_body(A, M, Ref, CA, CP),
     walk_body(B, M, Ref, CA, CP).
-walk_body(Lit, M, Ref, CA, CP1) :-
-    Lit = (AB; C),
-    member(AB, [(A->B), (A*->B)]),
+walk_body((A; B), M, Ref, CA, CP1) :-
     !,
-    prolog_current_choice(CP2),
-    SFlag = s([]),
-    findall(Lit,
-            ( member(Branch, [AB, C]),
-              walk_body_if_branch(Branch, M, Ref, CA, SFlag, CP1, CP2)
-            ), [Term|TermL]),
-    foldl(greatest_common_unifier, TermL, Term, Lit),
-    ( member(leave_cp1, FlagL)
-    ->true
-    ; prolog_cut_to(CP1)
-    ),
-    ( member(insert_cp, FlagL)
-    ->( true
-      ; fail
+    ( member(A, [(_->_), (_*->_)])
+    ->prolog_current_choice(CP2),
+      SFlag = s([]),
+      findall(Lit,
+              ( member(Branch, [A, B]),
+                walk_body_if_branch(Branch, M, Ref, CA, SFlag, CP1, CP2)
+              ), [Term|TermL]),
+      foldl(greatest_common_unifier, TermL, Term, Lit),
+      ( member(leave_cp1, FlagL)
+      ->true
+      ; prolog_cut_to(CP1)
+      ),
+      ( member(insert_cp, FlagL)
+      ->( true
+        ; fail
+        )
+      ; true
       )
-    ; true
+    ; ( prolog_current_choice(CP2),
+        walk_body(A, M, Ref, CA, CP2)
+      ; walk_body(B, M, Ref, CA, CP1)
+      )
     ).
 walk_body((A->B), M, Ref, CA, CP1) :-
     !,
@@ -205,12 +328,6 @@ walk_body((A*->B), M, Ref, CA, CP1) :-
     ( prolog_current_choice(CP2),
       walk_body(A, M, Ref, CA, CP2),
       walk_body(B, M, Ref, CA, CP1)
-    ).
-walk_body((A; B), M, Ref, CA, CP1) :-
-    !,
-    ( prolog_current_choice(CP2),
-      walk_body(A, M, Ref, CA, CP2)
-    ; walk_body(B, M, Ref, CA, CP1)
     ).
 walk_body(call(A), M, Ref, CA, _) :-
     !,
@@ -225,43 +342,23 @@ walk_body(\+ (A), M, Ref, CA, _) :-
 walk_body(true, _, _, _, _) :- !.
 walk_body(fail, _, _, _, _) :- !, fail.
 walk_body(false, _, _, _, _) :- !, fail.
-walk_body(Lit, M, _, _, _) :-
-    ( valid_prop_asr(Lit, M, _)
-    ->( ( functor(Lit, F, A),
-          functor(Term, F, A),
-          Lit =@= Term,
-          valid_prop_asr(Lit, M, Asr),
-          member(DetProp,
-                 [globprops:multi(_),
-                  globprops:non_det(_),
-                  globprops:nondet(_)
-                 ]),
-          prop_asr(glob, DetProp, _, Asr)
-        )
-      ->( true
-        ; fail
-        )
-      ; ( forall(valid_prop_asr(Lit, M, Asr),
-               ( member(DetProp,
-                        [globprops:det(_),
-                         globprops:semidet(_),
-                         globprops:is_det(_),
-                         globprops:no_choicepoints(_)
-                        ]),
-                 prop_asr(glob, DetProp, _, Asr)
-               ))
-        )
-      ->true
-      ; ( forall(valid_prop_asr(Lit, M, Asr),
-               ( member(NegProp,
-                        [globprops:fails(_),
-                         globprops:failure(_)
-                        ]),
-                 prop_asr(glob, NegProp, _, Asr)
-               ))
-        )
-      ->fail
+walk_body(Lit, M, Ref, _, _) :-
+    ( \+ predicate_property(M:Lit, defined)
+    ->print_message(
+          error,
+          at_location(clause(Ref),
+          format("Undefined predicate found: ~q", [Lit]))),
+      fail
+    ; true
+    ),
+    ( inferred_det(Lit, M, nodet) % don`t check inferred multi, but the nodet flag
+    ->( true
+      ; fail
       )
+    ; inferred_det(Lit, M, isdet)
+    ->true
+    ; inferred_det(Lit, M, fails)
+    ->fail
     ; predicate_property(M:Lit, built_in)
     ->( true
       ; fail
@@ -272,7 +369,7 @@ walk_body(Lit, M, _, _, _) :-
       ( ClauseL = []
       ->fail
       ; ClauseL = [_]
-      ->walk_call(ClauseL, Lit, M, noop)
+      ->walk_call(Lit, M, noop)
       ; % abstraction step: making this decidable by not analyzing if there is a
         % choice point introduced by the clause
         ( true % leave a choicepoint
