fff5e45123b723bbc15be771f9afd9e288aef3bf
MatildaMoro <matilda.moro@student.unife.it>
2019-11-06 10:27:13 +0100

    apping-pl

--- prolog/mapping.pl
+++ prolog/mapping.pl
@@ -1,5 +1,6 @@
-prolog2function(class(IRI), 'Class(IRI)').
+prolog2function(class(ListIRI), ClFunc):-
   % qua IRI sarà una stringa che deve essere aggiunt via append a 'Class'
+  append('Class', ListIRI, ClFunc). 
 
 prolog2function(datatype(IRI), 'Datatype(IRI)').
   %come sopra
@@ -15,48 +16,57 @@ prolog2function(namedIndividual(IRI), 'NamedIndividual(IRI)').
 prolog2function(anonymousIndividual(IRI), 'AnonymousIndividual(nodeID)'). %% ObjectPropertyAssertion ?
   %come sopra
 
-prolog2function(subClassOf(ClassExpression1, ClassExpression2), 'SubClassOf(axiomAnnotations,subClassExpression, superClassExpression)'):-
-  % per ora non consideriamo axiomAnnotation, append delle due classExpression
+prolog2function(subClassOf(ClassExpression1, ClassExpression2), SCFunc):- % per ora non consideriamo axiomAnnotation, append delle due classExpression
   classExpression2function(ClassExpressionTrill1,ClassExpressionFunctional1),
-  classExpression2function(ClassExpressionTrill2,ClassExpressionFunctional2),
-  %append SubClassOf ClassExpressionFunctional1 ClassExpressionFunctional2
-  .
+  classExpression2function(ClassExpressionTrill2,ClassExpressionFunctional2), %append SubClassOf ClassExpressionFunctional1 ClassExpressionFunctional2
+  append('SubClassOf',[ClassExpressionFunctional1, ClassExpressionFunctional2],SCFunc).
 
-               
 prolog2function(equivalentClasses(ListaClassExpression), ECFunc):-  %'EquivalentClasses(axiomAnnotations, ClassExpression, ClassExpression { ClassExpression } )'):-
   findall(CEF,(member(CE,ListaClassExpression),classExpression2function(CE,CEF)),L),
   appendClasses('EquivalentClasses(',L,ECFunc).
 
-prolog2function(disjointClasses(set(classExpression)), 'DisjointClasses(axiomAnnotations, ClassExpression, ClassExpression { ClassExpression })').
+prolog2function(disjointClasses(ListaClassExpression), DCFunc):- %'DisjointClasses(axiomAnnotations, ClassExpression, ClassExpression { ClassExpression })').
 % come equivalent
+  findall(CEF,(member(CE,ListaClassExpression),classExpression2function(CE,CEF)),L),
+  appendClasses('DisjointClasses(',L,DCFunc).
 
 prolog2function(disjointUnion(IRI,ListaClassExpression), ECFunc):- %'DisjointUnion(axiomAnnotations, Class disjointClassExpressions)'
  % disjointClassExpressions := ClassExpression ClassExpression { ClassExpression }).
-              % misto fra subClass e equivalentClasses
-              classExpression2function(IRI,ClassExpressionFunctional),
-              findall(CEF,(member(CE,ListaClassExpression),classExpression2function(CE,CEF)),L),
-              appendClasses('EquivalentClasses',[ClassExpressionFunctional|L],ECFunc).
+  % misto fra subClass e equivalentClasses
+  classExpression2function(IRI,ClassExpressionFunctional),
+  findall(CEF,(member(CE,ListaClassExpression),classExpression2function(CE,CEF)),L),
+  appendClasses('EquivalentClasses',[ClassExpressionFunctional|L],ECFunc).
 
-prolog2function(subPropertyOf(propertyExpression, objectPropertyExpression),'SubObjectPropertyOf (axiomAnnotations, subObjectPropertyExpression, superObjectPropertyExpression )
-                subObjectPropertyExpression := ObjectPropertyExpression | propertyExpressionChain
-                propertyExpressionChain := 'ObjectPropertyChain' '(' ObjectPropertyExpression ObjectPropertyExpression { ObjectPropertyExpression } ')'
-                superObjectPropertyExpression := ObjectPropertyExpression').
+prolog2function(subPropertyOf(PropertyExpression1, PropertyExpression2), SPFunc):- 
+  propertyExpression2function(PropertyExpressionTrill1,PropertyExpressionFunctional1),
+  propertyExpression2function(PropertyExpressionTrill2,PropertyExpressionFunctional2),
+  append('SubPropertyOf',[PropertyExpressionFunctional1, PropertyExpressionFunctional2],SPFunc).
+     
+prolog2function(equivalentProperties(ListaPropertyExpression)), EPFunc):- %'EquivalentObjectProperties(axiomAnnotations, ObjectPropertyExpression, ObjectPropertyExpression { ObjectPropertyExpression })').
+  findall(PEF,(menber(,PE,ListaPropertyExpression), propertyExpression2function(PE,PEF)),A),
+  appendProperty('EquivalentObjectProperties(', A, EPFunc).
                 
-prolog2function(equivalentProperties(set(propertyExpression)), 'EquivalentObjectProperties(axiomAnnotations, ObjectPropertyExpression, ObjectPropertyExpression { ObjectPropertyExpression })').
-% come equivalent classes ma su property
-                
-prolog2function(dijointProperties(set(propertyExpression)), 'DisjointObjectProperties(axiomAnnotations, ObjectPropertyExpression, ObjectPropertyExpression { ObjectPropertyExpression })').
-          % come disjoint classes ma su property
+prolog2function(dijointProperties(ListaPropertyExpression)), DPFunc):- %'DisjointObjectProperties(axiomAnnotations, ObjectPropertyExpression, ObjectPropertyExpression { ObjectPropertyExpression })').
+  % come disjoint classes ma su property
+  findall(PEF,(member(PE, ListaPropertyExpression), propertyExpression2function(PE,PEF)),A),
+  appendProperty('DisjointObjectProperties(', A, DPFunc).
 
-prolog2function(inverseProperties(objectPropertyExpression, objectPropertyExpression), 'InverseObjectProperties(axiomAnnotations, ObjectPropertyExpression, ObjectPropertyExpression)').
-        % come subclass
-prolog2function(propertyDomain(PropertyExpression, ClassExpression), PDF):- %'ObjectPropertyDomain(axiomAnnotations, ObjectPropertyExpression, ClassExpression)').
-          propertyExpression2funtion(PropertyExpression,PropertyExpressionF),
-          classExpression2function(ClassExpression,ClassExpressionF),
-          append('ObjectPropertyDomain',[PropertyExpressionF,ClassExpressionF],PDF).
+prolog2function(inverseProperties(ObjectPropertyExpression1, ObjectPropertyExpression2), IOPFunc):- %'InverseObjectProperties(axiomAnnotations, ObjectPropertyExpression, ObjectPropertyExpression)').
+  % come subclass
+  ObjectPropertyExpression2function(ObjectPropertyExpressionTrill1,ObjectPropertyExpressionFunctional1),
+  ObjectPropertyExpression2function(ObjectPropertyExpressionTrill2,ObjectPropertyExpressionFunctional2), %append SubClassOf ClassExpressionFunctional1 ClassExpressionFunctional2
+  append('InverseObjectProperties',[ObjectPropertyExpressionFunctional1, ObjectPropertyExpressionFunctional2],IOPFunc).
 
-prolog2function(propertyRange(propertyExpression, classExpression),'ObjectPropertyRange(axiomAnnotations, ObjectPropertyExpression, ClassExpression)').
-                
+prolog2function(propertyDomain(PropertyExpression, ClassExpression), OPDFunc):- %'ObjectPropertyDomain(axiomAnnotations, ObjectPropertyExpression, ClassExpression)').
+  propertyExpression2function(PropertyExpression,PropertyExpressionF),
+  classExpression2function(ClassExpression,ClassExpressionF),
+  append('ObjectPropertyDomain',[PropertyExpressionF,ClassExpressionF],OPDFunc).
+
+prolog2function(propertyRange(PropertyExpression, ClassExpression), OPRFunc) :- %'ObjectPropertyRange(axiomAnnotations, ObjectPropertyExpression, ClassExpression)').
+  propertyExpression2function(PropertyExpression,PropertyExpressionF),
+  classExpression2function(ClassExpression,ClassExpressionF),
+  append('ObjectPropertyRange',[PropertyExpressionF,ClassExpressionF],OPRFunc).
+
 prolog2function(functionalProperty(propertyExpression),'FunctionalObjectProperty(axiomAnnotations, ObjectPropertyExpression)').
                 
 prolog2function(inverseFunctionalProperty(objectPropertyExpression),'InverseFunctionalObjectProperty(axiomAnnotations, ObjectPropertyExpression').
@@ -98,28 +108,23 @@ prolog2function(ontologyImport(ontology, IRI),''). %?
                 
 prolog2function(ontologyVersionInfo(ontology, IRI),''). %?
 
-
-                
-
-
-
-
-
 classExpression2function(CE,CEF):- 
   iri(CE,CEF); objectIntersectionOf(CE,CEF);objectSomeValuesFrom(CE,CEF),!.
+
+propertyExpression2function(PE, PEF):-
+  iri(PE,PEF);
  
  iri(IRI,IRI) :- atomic(IRI).
  objectIntersectionOf(intersectionOf(CEs),ClassExpressionFL):-
    ClassExpressionF = 'ObjectIntersectionOf(',
    findall(CEF,(member(CE,ListaClassExpression),classExpression2function(CE,CEF)),L),
    % append stringhe in L su classExpressionF
-   appnedClasses(ClassExpressionF,L,ClassExpressionFL).
- 
+   appendClasses(ClassExpressionF,L,ClassExpressionFL).
  
  objectSomeValuesFrom(someValuesFrom(P,C),SVFFunc):-
   classExpression2function(C,CF),
- propertyExpression2funtion(P,PF),
- appendClasses('ObjectSomeValuesFrom(',[PF,CF], SVFFunc).
+  propertyExpression2funtion(P,PF),
+  appendClasses('ObjectSomeValuesFrom(',[PF,CF], SVFFunc).
 
 appendFunctional(Pred, Lista, Ris):-
   % 1 append fra Pred e (
