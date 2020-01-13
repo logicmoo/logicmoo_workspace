59b0140d9e12375233d2ae5bbc0c42f42837c477
Riccardo Zese <riccardo.zese@unife.it>
2020-01-13 15:44:13 +0100

    New example

--- prolog/examples/review_example.pl
+++ prolog/examples/review_example.pl
@@ -7,7 +7,7 @@ subClassOf(f,b).
 
 subClassOf(a,unionOf([b,c,d])).
 subClassOf(a,minCardinality(5,r)).
-subClassOf(c,maxCardinality(3,r)).
-subClassOf(d,maxCardinality(2,r)).
+subClassOf(c,maxCardinality(4,r)).
+subClassOf(d,maxCardinality(3,r)).
  
 % query: sub_class(a,b).
