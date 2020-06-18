149a260761fbe4c7737f87c0b040301dfc151fa3
Miguel Calejo <mc@interprolog.com>
2020-06-18 19:57:49 +0100

    Support loading SWISH server extensions, e.g. from outside Docker container

--- swish/user_module_file.pl
+++ swish/user_module_file.pl
@@ -1,6 +1,7 @@
 :- ensure_loaded(user_module_base).
 :- use_module(lps_server_UI).
 :- use_rendering(lps_server_UI).
+:- (exists_directory(extensions) -> ensure_loaded('extensions/extensions.pl'), writeln("Loaded extensions."); true).
  % place after the above loading so we don't use it...:
 :- ensure_loaded(term_expander).
 
