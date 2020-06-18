c48cf24fd8dc6e31c774f59a542e8a6d1cbe9c59
Miguel Calejo <mc@interprolog.com>
2020-06-18 20:12:09 +0100

    Tweak extension folder path

--- swish/user_module_file.pl
+++ swish/user_module_file.pl
@@ -1,7 +1,9 @@
 :- ensure_loaded(user_module_base).
 :- use_module(lps_server_UI).
 :- use_rendering(lps_server_UI).
-:- (exists_directory(extensions) -> ensure_loaded('extensions/extensions.pl'), writeln("Loaded extensions."); true).
+:- multifile user:file_search_path/2.
+user:file_search_path(lps_swish, lps_engine_dir('../swish')).
+:- absolute_file_name(lps_swish(extensions),ED), (exists_directory(ED) -> ensure_loaded(lps_swish('extensions/extensions.pl')), writeln("Loaded extensions."); true).
  % place after the above loading so we don't use it...:
 :- ensure_loaded(term_expander).
 
