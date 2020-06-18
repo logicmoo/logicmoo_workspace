3c96d8fe14ddd99a793ff685e3a5e8b04c1ff9c7
Miguel Calejo <mc@interprolog.com>
2020-06-18 20:21:04 +0100

    interim commit...

--- swish/user_module_file.pl
+++ swish/user_module_file.pl
@@ -3,7 +3,8 @@
 :- use_rendering(lps_server_UI).
 :- multifile user:file_search_path/2.
 user:file_search_path(lps_swish, lps_engine_dir('../swish')).
-:- absolute_file_name(lps_swish(extensions),ED), (exists_directory(ED) -> ensure_loaded(lps_swish('extensions/extensions.pl')), writeln("Loaded extensions."); true).
+:- absolute_file_name(lps_swish(extensions),ED), format("Extensions directory: ~a~n",[ED]), 
+	(exists_directory(ED) -> writeln("Loading..."), ensure_loaded(lps_swish('extensions/extensions.pl')), writeln("...user extensions."); true).
  % place after the above loading so we don't use it...:
 :- ensure_loaded(term_expander).
 
