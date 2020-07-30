0b921c1cc95e02bee91d63099b51362b1c55a3d2
Riccardo Zese <riccardo.zese@unife.it>
2020-07-30 11:38:33 +0200

    Version 5.2.2

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(trill).
 title('A tableau probabilistic reasoner in three different versions').
-version('5.2.1').
+version('5.2.2').
 author('Riccardo Zese', 'zsercr@unife.it').
 download('https://github.com/rzese/trill/releases/*.zip').
 requires(bddem).
