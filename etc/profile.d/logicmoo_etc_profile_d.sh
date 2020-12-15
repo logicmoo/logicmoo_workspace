f3ee73368b424bdef76d32bf85aceb808013fc77
logicmoo <logicmoo@gmail.com>
2020-12-14 16:09:34 -0800

    000-logicmoo.conf

--- etc/profile.d/logicmoo_etc_profile_d.sh
+++ etc/profile.d/logicmoo_etc_profile_d.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 
+#start_oo_redir {
+#sysctl net.ipv4.ip_forward=1
+#iptables -F FORWARD
+#iptables -t nat -A PREROUTING -p tcp --dport 58100 -j  REDIRECT --to-destination 10.0.0.122:8100
+#iptables -A FORWARD -p tcp -d 10.0.0.122 --dport 8100 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+##iptables -A POSTROUTING -t nat -p tcp -m tcp -s 10.0.0.122 --sport 8100 -j SNAT --to-source 10.0.0.180:58100
+#iptables -t nat -A POSTROUTING -j MASQUERADE
+#iptables -L
+#/ ssh root@localhost -L 127.0.0.1:58100:10.0.0.122:8100
+#}
+
 
 export WS_MAYBE=$PWD
 
