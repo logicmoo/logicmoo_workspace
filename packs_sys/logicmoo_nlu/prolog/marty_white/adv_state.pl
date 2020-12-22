f9821877e6266ecefb37ecaa42f83c4c37a5b202
logicmoo <logicmoo@gmail.com>
2020-12-21 19:55:34 -0800

    ID

--- packs_sys/logicmoo_nlu/prolog/marty_white/adv_state.pl
+++ packs_sys/logicmoo_nlu/prolog/marty_white/adv_state.pl
@@ -213,6 +213,8 @@ forget_always(Figment, M0, M1) :- select_always(Figment, M0, M1).
 % select_default(Figment, Default, M0, M1).
 thought(Figment, M) :- declared(Figment, M).
 
+unlistify(Figment0,Figment):- is_list(Figment0),!,must(Figment0=[Figment]).
+unlistify(Figment,Figment).
 
 in_agent_model(Agent, Fact, State):- in_model(Fact, State)*-> true ; (agent_thought_model(Agent, ModelData, State), in_model(Fact, ModelData)).
 
@@ -223,7 +225,8 @@ in_model0(E, L):- member(EE, L), same_element(EE, E).
 same_element(E, E) :- !.
 same_element(holds_at(E, T), E):- nonvar(T).
 
-
+agent_mem(_Agent,Mem0,Mem1,AMem0,AMem1):- Mem0=AMem0,Mem1=AMem1.
+agent_mem(_Agent,Mem0,AMem0):- Mem0=AMem0.
 
 :- defn_state_getter(agent_thought_model(agent, model)).
 agent_thought_model(Agent, ModelData, M0):- var(M0), get_advstate(State), !, member(memories(Agent, M0), State), agent_thought_model(Agent, ModelData, M0).
@@ -721,8 +724,8 @@ correct_some(Adjs,E,O):- check_atom(Adjs), must(correct_prop(sp(Adjs,E),O)).
 inner_dialog(Agent,Figment) :- is_list(Figment),!,forall(member(F,Figment),inner_dialog(Agent,F)).
 inner_dialog(Agent,Figment) :- notrace((format('~N',[]),in_color(pink,print_tree(inner_dialog(Agent,Figment))),format('~N',[]))).
 
-inner_dialog(Figment) :- is_list(Figment),!,forall(member(F,Figment),inner_dialog(Agent,F)).
-inner_dialog(Figment) :- notrace((format('~N',[]),in_color(pink,print_tree(inner_dialog(Agent,Figment))),format('~N',[]))).
+internal_dialog(Figment) :- is_list(Figment),!,forall(member(F,Figment),inner_dialog(Agent,F)).
+internal_dialog(Figment) :- notrace((format('~N',[]),in_color(pink,print_tree(inner_dialog(Agent,Figment))),format('~N',[]))).
 
 % for  the "TheSims" bot AI which will make the bots do what TheSims characters do... (they dont use the planner they use a simple priority routine)
 
