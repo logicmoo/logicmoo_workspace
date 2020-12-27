cc377be67ac119dd2e7df944283febb95b6cf92a
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2020-12-27 19:52:21 +0100

    fixed author name

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(sldnfdraw).
 title('SLDNF Draw is a Prolog program that draws SLDNF Trees in LaTeX').
 version('1.61').
-author('Maarco Gavanelli','marco.gavanelli@unife.it').
+author('Marco Gavanelli','marco.gavanelli@unife.it').
 
