cab1b57589cfcaaf788cbb0f436e4abedbf2d6e1
Miguel Calejo <mc@interprolog.com>
2021-02-01 20:47:14 +0000

    support negated atomic events

---
+++ examples/CLOUT_workshop/negatedEvents.pl
@@ -0,0 +1,22 @@
+% Masochistic agent
+maxTime(10).
+events shot.
+actions ouch, i_bet_you_cant_shoot_me.
+
+observe shot from 2.
+
+if shot then 
+	ouch.
+if not shot then 
+	i_bet_you_cant_shoot_me.
+
+if true then
+	not shot from 5,
+	writeln(inGoal) from _.
+
+
+/** <examples> 
+?- go(Timeline).
+*/
+
+
