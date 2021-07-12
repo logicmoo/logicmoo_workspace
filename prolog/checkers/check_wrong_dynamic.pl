5bed34503caf9f8027d978968aaa53e1e1594c59
Edison Mera <mera@process-design-center.com>
2021-07-12 11:24:18 +0200

    Fixed wrong dynamic warnings about is_pure_clause/2

--- prolog/checkers/check_wrong_dynamic.pl
+++ prolog/checkers/check_wrong_dynamic.pl
@@ -83,6 +83,7 @@ hide_var_dynamic_hook(unrtcheck2(_, _), rtchecks).
 hide_var_dynamic_hook(do_ac_head_prop_idx(_, _, _, _, _), assrt_comment).
 hide_var_dynamic_hook(bind_interface(_, _), interface).
 hide_var_dynamic_hook(unfold_goal(_, _), ref_scenarios).
+hide_var_dynamic_hook(is_pure_clause(_, _), pure).
 
 :- dynamic
     wrong_dynamic_db/4,
