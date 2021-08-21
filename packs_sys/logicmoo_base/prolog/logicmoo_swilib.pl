c3588444c358ec9f8e0f2970627e86c9eb699914
CasAM <dangerous.idiom@gmail.com>
2021-08-21 14:59:36 +0000

    Update logicmoo_swilib.pl

--- packs_sys/logicmoo_base/prolog/logicmoo_swilib.pl
+++ packs_sys/logicmoo_base/prolog/logicmoo_swilib.pl
@@ -2,16 +2,16 @@
 % ===================================================================
     File:         'logicmoo_swilib.'
     Purpose:       To load the logicmoo libraries as needed
-    Contact:       $Author: dmiles $@users.sourceforge.net ;
+    Contact:       $Author: dmiles logicmoo@gmail.com ;
     Version:       'logicmoo_swilib.' 1.0.0
     Revision:      $Revision: 1.7 $
-    Revised At:    $Date: 2002/07/11 21:57:28 $
+    Revised At:    $Date: 2021/07/11 21:57:28 $
     Author:        Douglas R. Miles
     Maintainers:   logicmoo
     E-mail:        logicmoo@gmail.com
     WWW:           http://www.prologmoo.com
     SCM:           https://github.com/logicmoo/PrologMUD/tree/master/pack/logicmoo_base
-    Copyleft:      1999-2015, LogicMOO Prolog Extensions
+    Copyleft:      1999-2021, LogicMOO Prolog Extensions
     License:       Lesser GNU Public License
 % ===================================================================
 */
