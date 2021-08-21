1fce926c9698dcab6e1071f8ab9f94f47697d9a4
CasAM <dangerous.idiom@gmail.com>
2021-08-21 13:47:34 +0000

    Update logicmoo_dec.pl

--- packs_sys/logicmoo_ec/prolog/logicmoo_dec.pl
+++ packs_sys/logicmoo_ec/prolog/logicmoo_dec.pl
@@ -1,4 +1,9 @@
 :- module(logicmoo_dec,[test_logicmoo_dec/0]).
+/** <module> MODULE LOGICMOO DEC
+This module holds Eric Mullers DEC reasoner E-file interface for LOGICMOO.
+@author Douglas R. Miles
+@license LGPL
+*/
 
 % [Required] Load the Logicmoo Library Utils
 :- ensure_loaded(library(logicmoo_common)).
