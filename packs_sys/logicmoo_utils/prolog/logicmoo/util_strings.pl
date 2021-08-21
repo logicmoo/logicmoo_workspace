b1f8262da06ea0d859ecd1ed68a1b1f2701d181d
CasAM <dangerous.idiom@gmail.com>
2021-08-21 15:02:00 +0000

    Update util_strings.pl

--- packs_sys/logicmoo_utils/prolog/logicmoo/util_strings.pl
+++ packs_sys/logicmoo_utils/prolog/logicmoo/util_strings.pl
@@ -2,10 +2,10 @@
 % File 'logicmoo_util_strings.pl'
 % Purpose: Common Logicmoo library Functions for Strings
 % Maintainers: Douglas Miles/Annie Ogborn/Kino Coursey
-% Contact: $Author: dmiles $@users.sourceforge.net ;
+% Contact: $Author: logicmoo@gmail.com;
 % Version: 'logicmoo_util_strings.pl' 1.0.0
 % Revision:  $Revision: 1.7 $
-% Revised At:   $Date: 2002/07/11 21:57:28 $
+% Revised At:   $Date: 2021/07/11 21:57:28 $
 % ===================================================================
 
 % File: /opt/PrologMUD/pack/logicmoo_base/prolog/logicmoo/util/logicmoo_util_strings.pl
