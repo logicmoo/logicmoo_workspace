40378d51f20e38b11149f54846afe2dbfd53f5ac
CasAM <dangerous.idiom@gmail.com>
2021-08-21 14:38:59 +0000

    Update logicmoo_utils.pl

--- packs_sys/logicmoo_utils/prolog/logicmoo_utils.pl
+++ packs_sys/logicmoo_utils/prolog/logicmoo_utils.pl
@@ -1,6 +1,6 @@
 /* Part of LogicMOO Base Logicmoo Path Setups
 % ===================================================================
-    File:         'logicmoo_util_library.pl'
+    Previous File:         'logicmoo_util_library.pl'
     Purpose:       To load the logicmoo libraries as needed
     Contact:       $Author: dmiles $@users.sourceforge.net ;
     Version:       'logicmoo_util_library.pl' 1.0.0
@@ -9,9 +9,9 @@
     Author:        Douglas R. Miles
     Maintainers:   logicmoo
     E-mail:        logicmoo@gmail.com
-    WWW:           http://www.prologmoo.com
+    WWW:           http://www.logicmoo.org
     SCM:           https://github.com/logicmoo/PrologMUD/tree/master/pack/logicmoo_base
-    Copyleft:      1999-2015, LogicMOO Prolog Extensions
+    Copyleft:      1999-2021, LogicMOO Prolog Extensions
     License:       Lesser GNU Public License
 % ===================================================================
 */
@@ -19,8 +19,8 @@
 :- module(logicmoo_utils,[]).
 /** <module> Utility LOGICMOO UTILS
 This module holds less common utils that have to be loaded individually.  
-@author Douglas R. Miles
-@license LGPL
+ - @author Douglas R. Miles
+ - @license LGPL
 */
 
 %:- discontiguous(logicmoo_utils:'$exported_op'/3).
