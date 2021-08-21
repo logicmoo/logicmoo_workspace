3fdc03bc029cf6f3e0c7253e856205c49120897c
CasAM <dangerous.idiom@gmail.com>
2021-08-21 14:52:51 +0000

    Update must_sanity.pl

--- packs_sys/logicmoo_utils/prolog/must_sanity.pl
+++ packs_sys/logicmoo_utils/prolog/must_sanity.pl
@@ -3,10 +3,10 @@
 % File '$FILENAME.pl'
 % Purpose: An Implementation in SWI-Prolog of certain debugging tools
 % Maintainer: Douglas Miles
-% Contact: $Author: dmiles $@users.sourceforge.net ;
+% Contact: $Author: dmiles logicmoo@gmail.com ;
 % Version: '$FILENAME.pl' 1.0.0
 % Revision: $Revision: 1.1 $
-% Revised At:  $Date: 2002/07/11 21:57:28 $
+% Revised At:  $Date: 2021/07/11 21:57:28 $
 % Licience: LGPL
 % ===================================================================
 */
