bf94ae7fb98f20c6765cab9b615c6203ca01e83d
Wiki manager <wiki@swi-prolog.org>
2021-08-22 20:41:51 -0700

    junit_tmp6

--- test_on_release.sh
+++ test_on_release.sh
@@ -20,7 +20,8 @@ echo "<testsuites>" > $TESTING_TEMP/junit.xml
 find $TESTING_TEMP -name "Report-*.xml" -exec sed -e "s/<testsuites>//g" -e "s|</testsuites>||g" {} >> $TESTING_TEMP/junit.xml  \;
 echo "</testsuites>" >> $TESTING_TEMP/junit.xml
 
-echo "COPY .. cp $TESTING_TEMP/junit.xml /tmp/junit.xml"
-cp -f $TESTING_TEMP/junit.xml .git/junit.xml
 
+cp -f $TESTING_TEMP/junit.xml .git/junit.xml
+touch .git/junit.xml
+head .git/junit.xml
 
