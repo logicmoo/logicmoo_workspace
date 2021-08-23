32b0965e71e28db3a81dd273c6ed9f81cb734139
Wiki manager <wiki@swi-prolog.org>
2021-08-23 12:59:16 -0700

    build all

--- test_on_release.sh
+++ test_on_release.sh
@@ -2,6 +2,8 @@
 
 source ./logicmoo_env.sh -v
 
+WAS_PWD=$PWD
+
 unset TESTING_TEMP
 
 [ -z "$TESTING_TEMP" ] && [ -d "$(pwd)/test_results" ] && export TESTING_TEMP=$(pwd)/test_results/$(whoami)
@@ -20,7 +22,7 @@ fi
 echo -e "Running release (all) tests\nTESTING_TEMP=$TESTING_TEMP\n( cd $PWD ; $BASH_SOURCE $TEST_PARAMS )"
 
 # base ./packs_sys/pfc/t
-find ./packs_sys/pfc/t -mindepth 2 -name "test_on_*.sh" -execdir bash -c "source '{}' \"$TEST_PARAMS\"" \;
+find $WAS_PWD -mindepth 2 -name "test_on_*.sh" -execdir bash -c "source '{}' \"$TEST_PARAMS\"" \;
 
 #echo "<testsuites>" > $TESTING_TEMP/junit.xml
 #find $TESTING_TEMP -name "Report-*.xml" -exec sed -e "s/<testsuites>//g" -e "s|</testsuites>||g" {} >> $TESTING_TEMP/junit.xml  \;
