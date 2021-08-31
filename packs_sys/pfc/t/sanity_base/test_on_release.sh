f7ee30ad462bc49371bc3275c404e62381b5d89f
Wiki manager <wiki@swi-prolog.org>
2021-08-31 04:26:20 -0700

    less tests

--- packs_sys/pfc/t/sanity_base/test_on_release.sh
+++ packs_sys/pfc/t/sanity_base/test_on_release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
 #source `which junit_prolog.sh`
-source /opt/logicmoo_workspace/bin/junit_prolog.sh "$*"
+source /opt/logicmoo_workspace/bin/junit_prolog.sh "*01.*"
+#source /opt/logicmoo_workspace/bin/junit_prolog.sh "$*"
 
