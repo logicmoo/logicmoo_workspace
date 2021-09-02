68cfc5fee5af94ee2a3f1a3cd52948ca2319ba52
CasAM <dangerous.idiom@gmail.com>
2021-09-02 15:11:57 +0000

    Update nb_set_term.pl

--- packs_sys/logicmoo_utils/prolog/logicmoo/nb_set_term.pl
+++ packs_sys/logicmoo_utils/prolog/logicmoo/nb_set_term.pl
@@ -1,8 +1,12 @@
 :- module(nb_set_term, [nb_set_has/2, nb_set_add/2, nb_set_add1/2, nb_set_rem/2, nb_set_rem1/2]).
-/** <module> Utility LOGICMOO_NB_SET_TERM
-This module allows non-backtrackable manipulation of prolog data.
-@author Douglas R. Miles
-@license LGPL
+/** <module> Utility LOGICMOO NB SET TERM
+This module allows non-backtrackable manipulation of prolog data. 
+Manipulate and edit terms without worrying about them reverting. 
+
+- @author Douglas R. Miles
+- @license LGPL 
+*/
+
 */
 :- set_module(class(library)).
 /*  Logicmoo Debug Tools
