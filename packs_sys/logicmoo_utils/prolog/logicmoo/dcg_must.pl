044965acb9ebf2893366a284eca99a2888ca190c
CasAM <dangerous.idiom@gmail.com>
2021-09-02 14:53:18 +0000

    Update dcg_must.pl

--- packs_sys/logicmoo_utils/prolog/logicmoo/dcg_must.pl
+++ packs_sys/logicmoo_utils/prolog/logicmoo/dcg_must.pl
@@ -3,10 +3,11 @@
 % File 'dcg_meta.pl'
 % Purpose: An Implementation in SWI-Prolog of certain debugging tools
 % Maintainer: Douglas Miles
-% Contact: $Author: dmiles $@users.sourceforge.net ;
+% Contact: $Author: dmiles logicmoo@gmail.com ;
 % Version: 'logicmoo_util_bugger.pl' 1.0.0
 % Revision:  $Revision: 1.1 $
-% Revised At:   $Date: 2002/07/11 21:57:28 $
+% Created: $Date: 2002/07/11 21:57:28 $
+% Revised At:   $Date: 2021/07/11 21:57:28 $
 % ===================================================================
 */
 :- module(dcg_must,[
