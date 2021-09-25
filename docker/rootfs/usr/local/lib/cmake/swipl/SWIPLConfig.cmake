da7955ea0c6fffd1628827c08abfd181e72b32f9
TeamSPoon <logicmoo@gmail.com>
2021-09-25 01:33:53 -0700

    debian10-bin

--- docker/rootfs/usr/local/lib/cmake/swipl/SWIPLConfig.cmake
+++ docker/rootfs/usr/local/lib/cmake/swipl/SWIPLConfig.cmake
@@ -11,8 +11,8 @@ get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" 
 set(SWIPL_VERSION_COUNT 3)
 set(SWIPL_VERSION_MAJOR 8)
 set(SWIPL_VERSION_MINOR 3)
-set(SWIPL_VERSION_PATCH 28)
-set(SWIPL_VERSION_STRING 8.3.28)
+set(SWIPL_VERSION_PATCH 29)
+set(SWIPL_VERSION_STRING 8.3.29)
 
 # Location suitable for passing to PL_initialise() as argv[0]
 set(SWIPL_EXECUTABLE "${PACKAGE_PREFIX_DIR}/lib/swipl/bin/x86_64-linux/swipl")
