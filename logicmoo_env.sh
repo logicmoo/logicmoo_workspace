54ed84c81bcc5b3b735cc5e364d8c441591ff1b0
Douglas Miles <logicmoo@gmail.com>
2021-11-09 03:51:47 +0000

    Update logicmoo_env.sh

--- logicmoo_env.sh
+++ logicmoo_env.sh
@@ -7,7 +7,7 @@ export BIN_CLASS_DIR=~prologmud_server/classes
 export CLASSPATH="$BIN_CLASS_DIR:/usr/share/java/*"
 
 ECHO=MAINTAINER
-export SWI_TAG=e68098ba2b38d7d9597def1b4fc0a8cef1ed43cb
+# export SWI_TAG=e68098ba2b38d7d9597def1b4fc0a8cef1ed43cb
 
 if [[ "${1}" == "-q" ]] ;then
    ECHO=MAINTAINER
