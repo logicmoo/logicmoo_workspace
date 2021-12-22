7017e1db9afcd99707bac5eb2671f3c0b14df518
Douglas R Miles <logicmoo@gmail.com>
2021-12-21 16:20:08 -0800

    ../../../../logicmoo_nars/prolog/nars/nal.pl

---
+++ packs_sys/logicmoo_nars/prolog/nars/nal.pl
@@ -0,0 +1,15 @@
+% nal.pl
+% Non-Axiomatic Logic in Prolog
+% Version: 1.1, September 2012
+% GNU Lesser General Public License
+% Author: Pei Wang/Pat Hammer/Douglas Miles
+
+:- module(nal, []).
+
+:- set_module(class(library)).
+:- set_module(base(system)).
+
+:- use_module(library(nars/nal_reader)). 
+
+:- ensure_loaded(nars).
+
