f7dc476478a49a06b53afc515e5ea6accc6515ff
Douglas R Miles <logicmoo@gmail.com>
2022-06-04 03:12:33 -0700

    comparitor4 size

--- packs_sys/logicmoo_agi/prolog/kaggle_arc/kaggle_arc_howdiff.pl
+++ packs_sys/logicmoo_agi/prolog/kaggle_arc/kaggle_arc_howdiff.pl
@@ -134,7 +134,7 @@ print_info(A):- pt(A).
 
 o2g(Obj,Glyph):- object_glyph(Obj,Glyph).
 o2c(Obj,Glyph):- color(Obj,Glyph).
-o2ansi(Obj,O):- o2c(Obj,C),o2g(Obj,G),wots(S,color_print(C,G)),format(atom(O),"' ~w '",[S]).
+o2ansi(Obj,S):- o2c(Obj,C),o2g(Obj,G),atom_concat_list([' ',G,' '],O),!,sformat(F,'~q',[O],wots(S,color_print(C,F)).
 :- dynamic(g2o/2).
 
 into_obj(G,_):- plain_var(G),!,fail.
